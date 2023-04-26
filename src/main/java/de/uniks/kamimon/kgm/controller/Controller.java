884fb6f7b7af39ef9a3289c7df8b10321cc4b010
discobreak21 <bastianbaer@hotmail.com>
2023-04-26 17:32:08 +0200

    feat: Add Controller class

---
+++ src/main/java/de/uniks/kamimon/kgm/controller/Controller.java
@@ -0,0 +1,33 @@
+package de.uniks.kamimon.kgm.controller;
+
+import de.uniks.kamimon.kgm.Main;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+
+public abstract class Controller {
+    public void init(){
+
+    }
+
+    public void destroy(){
+
+    }
+
+    public Parent render(){
+        return load(getClass().getSimpleName().replace("Controller", ""));
+    }
+
+    protected Parent load(String view){
+        final FXMLLoader loader = new FXMLLoader(Main.class.getResource("views/" + view + ".fxml"));
+        loader.setControllerFactory(c -> this);
+        //TODO: will be implemented after lecture 05
+        //loader.setResourcess(resources);
+        try{
+            return loader.load();
+        }catch (IOException exception){
+            throw new RuntimeException(exception);
+        }
+    }
+}
