842fc89525f61bd2415c20d047792e7137dbd3a2
discobreak21 <bastianbaer@hotmail.com>
2023-04-27 08:29:53 +0200

    feat: App icon & taskbar icon
    Both icons are implemented as in Lecture, but don't
    show
    
    TODO: make them show

--- src/main/java/de/uniks/kamimon/kgm/App.java
+++ src/main/java/de/uniks/kamimon/kgm/App.java
@@ -4,7 +4,12 @@ import de.uniks.kamimon.kgm.controller.Controller;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.util.Objects;
 
 public class App extends Application {
     private Stage stage;
@@ -31,9 +36,35 @@ public class App extends Application {
         stage.setScene(scene);
         stage.show();
 
+        //shows in the top bar of the app window (very tiny)
+        setAppIcon(stage);
+        //icon in the taskbar of the os
+        setTaskbarIcon();
+
         if(controller != null){
             initAndRender(controller);
             return;
+        }
+    }
+
+    private void setAppIcon(Stage stage){
+        //requireNonNull was not shown in Lecture, but is needed to eliminate warning
+        final Image image = new Image(Objects.requireNonNull(App.class.getResource("icon.png")).toString());
+        stage.getIcons().add(image);
+    }
+
+    private void setTaskbarIcon(){
+        if(GraphicsEnvironment.isHeadless()){
+            //No Taskbar Icon, if headless -> is important for tests
+            return;
+        }
+        try{
+            final Taskbar taskbar = Taskbar.getTaskbar();
+            //requireNonNull was not shown in Lecture, but is needed to eliminate warning
+            final java.awt.Image image = ImageIO.read(Objects.requireNonNull(Main.class.getResource("icon.png")));
+            taskbar.setIconImage(image);
+        }catch (Exception ignored){
+
         }
     }
     @Override
