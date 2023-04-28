efb1b3143a0f02b43e0cf568f8b826a826962d1c
Lukas1311 <lukas.lingmann@web.de>
2023-04-28 18:19:47 +0200

    fix: Add FXML annotation

--- src/main/java/de/uniks/kamimon/kgm/controller/FriendController.java
+++ src/main/java/de/uniks/kamimon/kgm/controller/FriendController.java
@@ -1,5 +1,6 @@
 package de.uniks.kamimon.kgm.controller;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
@@ -7,10 +8,15 @@ import javafx.scene.text.Text;
 
 public class FriendController extends Controller {
 
+    @FXML
     public VBox avatarBox;
+    @FXML
     public Text userName;
+    @FXML
     public Circle userStatus;
+    @FXML
     public Button messageButton;
+    @FXML
     public Button removeFriendButton;
 
 
