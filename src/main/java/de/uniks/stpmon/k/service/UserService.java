b7a469e2f105c51abbf3e4916a1572e019921065
discobreak21 <bastianbaer@hotmail.com>
2023-04-28 10:43:45 +0200

    feat: UserService
    WIP:  AuthService is needed to get id of current user

--- src/main/java/de/uniks/stpmon/k/service/UserService.java
+++ src/main/java/de/uniks/stpmon/k/service/UserService.java
@@ -1,4 +1,20 @@
 package de.uniks.stpmon.k.service;
 
+import de.uniks.stpmon.k.dto.User;
+import de.uniks.stpmon.k.rest.UserApiService;
+import io.reactivex.rxjava3.core.Observable;
+
+import javax.inject.Inject;
+
 public class UserService {
+    private final UserApiService userApiService;
+
+    @Inject
+    public UserService(UserApiService userApiService){
+        this.userApiService = userApiService;
+    }
+
+    public Observable<User> setUsername(String username){
+        //Auth Service is needed to get id of user
+    }
 }
