cc1e6ec2a2d4b00f56584955daf3c86bd9ec744e
Lukas1311 <lukas.lingmann@web.de>
2023-04-28 18:08:20 +0200

    feat: Add FriendController

---
+++ src/main/java/de/uniks/kamimon/kgm/controller/FriendController.java
@@ -0,0 +1,24 @@
+package de.uniks.kamimon.kgm.controller;
+
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+
+public class FriendController extends Controller {
+
+    public VBox avatarBox;
+    public Text userName;
+    public Circle userStatus;
+    public Button messageButton;
+    public Button removeFriendButton;
+
+
+    public void openChat() {
+
+    }
+
+    public void removeFriend() {
+
+    }
+}
