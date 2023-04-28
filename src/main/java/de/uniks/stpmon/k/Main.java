9bd25ccfb13601f7660ae75ff0b2d4652d9db42e
discobreak21 <bastianbaer@hotmail.com>
2023-04-28 09:08:04 +0200

    feat: HttpModule init

--- src/main/java/de/uniks/stpmon/k/Main.java
+++ src/main/java/de/uniks/stpmon/k/Main.java
@@ -3,6 +3,7 @@ package de.uniks.stpmon.k;
 import javafx.application.Application;
 
 public class Main {
+    public static final String API_URL = "https://stpmon.uniks.de/api/v1";
     public static void main(String[] args){
         Application.launch(App.class, args);
     }
---
+++ src/main/java/de/uniks/stpmon/k/service/UserApiService.java
@@ -0,0 +1,4 @@
+package de.uniks.stpmon.k.service;
+
+public class UserApiService {
+}
