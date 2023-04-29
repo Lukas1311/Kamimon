977d9d0fb6cbd474731c80c0aa38f297ea2e6030
IsmailHalli <uk083810@student.uni-kassel.de>
2023-04-29 13:51:58 +0200

    feat: Add RegionListController

---
+++ src/main/java/de/uniks/stpmon/k/controller/RegionListController.java
@@ -0,0 +1,11 @@
+package de.uniks.stpmon.k.controller;
+
+import javax.inject.Inject;
+
+public class RegionListController extends Controller{
+    //is needed for dagger
+    @Inject
+    public RegionListController(){
+
+    }
+}
