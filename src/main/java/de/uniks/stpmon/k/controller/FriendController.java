5acceb1c1c3c11e3531f2c20f743bfbd8d220065
Lukas1311 <lukas.lingmann@web.de>
2023-04-29 10:12:15 +0200

    feat: Add dummy User

--- src/main/java/de/uniks/stpmon/k/controller/FriendController.java
+++ src/main/java/de/uniks/stpmon/k/controller/FriendController.java
@@ -1,10 +1,15 @@
 package de.uniks.stpmon.k.controller;
 
+import de.uniks.stpmon.k.dto.User;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
+
+import java.util.ArrayList;
 
 public class FriendController extends Controller {
 
@@ -19,6 +24,35 @@ public class FriendController extends Controller {
     @FXML
     public Button removeFriendButton;
 
+    private final User user;
+
+    public FriendController() {
+        this(new User("", "", "0", "Dummy", "offline", "", new ArrayList<>()));
+    }
+
+    public FriendController(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Parent render() {
+        final Parent parent = super.render();
+
+        userName.setText(user.name());
+
+        if (user.status().equals("offline")) {
+            userStatus.setFill(Color.RED);
+        }
+
+        //add avatar-url when avatar != null
+        //avatarBox.setBackground(new Background(new BackgroundImage(new Image(""), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
+
+        messageButton.setOnAction(e -> openChat());
+
+        removeFriendButton.setOnAction(e -> removeFriend());
+
+        return parent;
+    }
 
     public void openChat() {
 
