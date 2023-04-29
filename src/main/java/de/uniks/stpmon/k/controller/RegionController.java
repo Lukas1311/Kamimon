eedc2d6204447c4be3dba6e37c8190a056e47473
IsmailHalli <uk083810@student.uni-kassel.de>
2023-04-29 15:31:27 +0200

    feat: Add IngameController
    WIP: It is useless

--- src/main/java/de/uniks/stpmon/k/controller/RegionController.java
+++ src/main/java/de/uniks/stpmon/k/controller/RegionController.java
@@ -3,10 +3,10 @@ package de.uniks.stpmon.k.controller;
 import de.uniks.stpmon.k.App;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Region;
 
 import javax.inject.Inject;
-import java.awt.*;
 
 public class RegionController extends Controller{
     private final Region region;
@@ -23,7 +23,10 @@ public class RegionController extends Controller{
     public Parent render(){
         final Parent parent = super.render();
         //TODO: I need the Model to get the name of the region. I set the Label of the Button with the Id just to look if it works.
-        regionButton.setLabel(region.getId());
+        regionButton.setText(region.getId());
+        regionButton.setOnAction(event -> {
+            app.show(new IngameController(app));
+        });
         return parent;
     }
 }
