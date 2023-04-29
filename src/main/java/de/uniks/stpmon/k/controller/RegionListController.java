b23133ce4876a59ef54dac83eeb3165079537c57
IsmailHalli <uk083810@student.uni-kassel.de>
2023-04-29 15:12:41 +0200

    feat: RegionListController render

--- src/main/java/de/uniks/stpmon/k/controller/RegionListController.java
+++ src/main/java/de/uniks/stpmon/k/controller/RegionListController.java
@@ -1,20 +1,45 @@
 package de.uniks.stpmon.k.controller;
 
+import de.uniks.stpmon.k.App;
+import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
 
 import javax.inject.Inject;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RegionListController extends Controller{
+    private final App app;
+    private final List<Region> regionsList;
+    private final List<RegionController> controllers = new ArrayList<>();
+    @FXML
+    private AnchorPane regions;
+
     //is needed for dagger
     @Inject
-    public RegionListController(){
-
+    public RegionListController(App app, List<Region> regionsList){
+        this.app = app;
+        this.regionsList = regionsList;
 
     }
 
     @Override
     public Parent render(){
         final Parent parent = super.render();
+        regions.getChildren().clear();
+        for(final Region region: regionsList) {
+            final RegionController regionController = new RegionController(app, region);
+            controllers.add(regionController);
+            regionController.init();
+            try {
+                regions.getChildren().add(regionController.render());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
         return parent;
     }
 }
