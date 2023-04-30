b10079ea25ae266dc7a5e23831d09219d05b5981
halber <XhalberX@gmail.com>
2023-04-30 19:40:09 +0200

    feat: Add inject App -> Every Controller can use app

--- src/main/java/de/uniks/stpmon/k/controller/Controller.java
+++ src/main/java/de/uniks/stpmon/k/controller/Controller.java
@@ -1,12 +1,16 @@
 package de.uniks.stpmon.k.controller;
 
+import de.uniks.stpmon.k.App;
 import de.uniks.stpmon.k.Main;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 
+import javax.inject.Inject;
 import java.io.IOException;
 
 public abstract class Controller {
+    @Inject
+    protected App app;
     public void init(){
 
     }
