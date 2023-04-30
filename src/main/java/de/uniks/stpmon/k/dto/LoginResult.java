48ccc290be3df7122a3fc1a17577eee39458f0ce
sh0xy <53441989+sh0xy@users.noreply.github.com>
2023-04-30 13:57:20 +0200

    feat: update LoginResult dto

--- src/main/java/de/uniks/stpmon/k/dto/LoginResult.java
+++ src/main/java/de/uniks/stpmon/k/dto/LoginResult.java
@@ -1,6 +1,12 @@
 package de.uniks.stpmon.k.dto;
 
+import java.util.ArrayList;
+
 public record LoginResult(
+    // String name,
+    // String status,
+    // String avatar,
+    // ArrayList<String> friends,
     String accessToken,
     String refreshToken
 ) {
