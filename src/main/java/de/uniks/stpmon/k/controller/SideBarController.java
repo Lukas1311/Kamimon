74d6ef69d07db62aced33d6175a52624009c1b5e
halber <XhalberX@gmail.com>
2023-04-30 19:08:35 +0200

    feat: init SideBarController

---
+++ src/main/java/de/uniks/stpmon/k/controller/SideBarController.java
@@ -0,0 +1,27 @@
+package de.uniks.stpmon.k.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+public class SideBarController {
+
+    @FXML
+    public Button chat;
+    @FXML
+    public Button friends;
+    @FXML
+    public Button home;
+    @FXML
+    public VBox vBox;
+
+    public void openChat() {
+    }
+
+    public void openFriends() {
+    }
+
+    public void backtoLobby() {
+    }
+}
