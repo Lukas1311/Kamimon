e2e34dc74e9ab49c84bd40fb52c69f9d17ecd898
Lukas1311 <lukas.lingmann@web.de>
2023-04-30 17:08:32 +0200

    feat: dummy FriendList

--- src/main/java/de/uniks/stpmon/k/controller/FriendListController.java
+++ src/main/java/de/uniks/stpmon/k/controller/FriendListController.java
@@ -1,13 +1,19 @@
 package de.uniks.stpmon.k.controller;
 
 import de.uniks.stpmon.k.dto.User;
+import de.uniks.stpmon.k.rest.UserApiService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
+import javax.inject.Inject;
 import java.util.ArrayList;
+import java.util.List;
 
 public class FriendListController extends Controller {
     @FXML
@@ -17,25 +23,46 @@ public class FriendListController extends Controller {
     @FXML
     public Button searchButton;
 
-    private final User user;
+//    @Inject
+//    UserApiService userApiService;
+
+//    private final ObservableList<User> friends = FXCollections.observableArrayList();
+
+    private final List<FriendController> friendControllers = new ArrayList<>();
+
+    private final ArrayList<String> friends = new ArrayList<>();
+    private final User user = new User("", "", "0", "Alice", "online", "", friends);
 
 
     public FriendListController() {
-        this(new User("", "", "0", "Alice", "online", "", new ArrayList<>()));
+        friends.add("Dummy1");
+        friends.add("Dummy2");
+        friends.add("Dummy3");
+        friends.add("Dummy4");
     }
 
-    public FriendListController(User user) {
-        this.user = user;
-    }
+//    public FriendListController(User user) {
+//        this.user = user;
+//    }
 
+
+//    @Override
+//    public void init() {
+//        disposable.add(userApiService.getUsers().subscribe(this.friends::setAll));
+//    }
 
     @Override
     public Parent render() {
         final Parent parent = super.render();
 
+//        final ListView<User> friends = new ListView<>(this.friends);
+
+
         for (String user : user.friends()) {
             final FriendController friendController = new FriendController();
+            friendControllers.add(friendController);
             friendList.getChildren().add(friendController.render());
+            friendController.init();
         }
 
         searchButton.setOnAction(e -> searchForFriend());
@@ -48,4 +75,10 @@ public class FriendListController extends Controller {
         //TODO: create search function
     }
 
+    @Override
+    public void destroy() {
+        for (final FriendController i : friendControllers) {
+            i.destroy();
+        }
+    }
 }
