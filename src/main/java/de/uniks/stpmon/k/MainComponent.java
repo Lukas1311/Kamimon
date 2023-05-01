0b294e50b861b904e00827173634030af977c7fa
sh0xy <53441989+sh0xy@users.noreply.github.com>
2023-05-01 14:17:15 +0200

    feat: add auth service

--- src/main/java/de/uniks/stpmon/k/MainComponent.java
+++ src/main/java/de/uniks/stpmon/k/MainComponent.java
@@ -3,14 +3,14 @@ package de.uniks.stpmon.k;
 import dagger.BindsInstance;
 import dagger.Component;
 import de.uniks.stpmon.k.controller.LoginController;
+import de.uniks.stpmon.k.service.AuthenticationService;
 
 import javax.inject.Singleton;
 
 @Component(modules = {MainModule.class, HttpModule.class, PrefModule.class})
 @Singleton
 public interface MainComponent {
-    //TODO: LoginService not implemented yet
-    //LoginService loginService();
+    AuthenticationService authenticationService();
     LoginController loginController();
 
     //TODO: LobbyController not implemented yet
