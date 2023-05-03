88ff5fa3b7aab7f447088f202d53b747f6fed8e5
sh0xy <53441989+sh0xy@users.noreply.github.com>
2023-05-03 21:45:37 +0200

    refactor: add API_DOMAIN for use in NetworkAvailability

--- src/main/java/de/uniks/stpmon/k/Main.java
+++ src/main/java/de/uniks/stpmon/k/Main.java
@@ -3,7 +3,8 @@ package de.uniks.stpmon.k;
 import javafx.application.Application;
 
 public class Main {
-    public static final String API_URL = "https://stpmon.uniks.de/api/v1";
+    public static final String API_DOMAIN = "stpmon.uniks.de";
+    public static final String API_URL = "https://" + API_DOMAIN + "/api/v1";
     public static void main(String[] args){
         Application.launch(App.class, args);
     }
