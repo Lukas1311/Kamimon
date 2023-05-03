67b4e1e110e5a4130ad9a2f2b9224b1a5039e5f8
Alexander Haar <uk077258@student.uni-kassel.de>
2023-05-03 17:53:51 +0200

    feat: init LobbyController with toIngame Function (Placeholer)

---
+++ src/main/java/de/uniks/stpmon/k/controller/LobbyController.java
@@ -0,0 +1,32 @@
+package de.uniks.stpmon.k.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+
+public class LobbyController extends Controller {
+    @FXML
+    public Button ingame;
+    @FXML
+    public Pane pane;
+    @Inject
+    Provider<HybridController> hybridController;
+
+    @Inject
+    LobbyController() {
+    }
+
+    @Override
+    public Parent render(){
+        final Parent parent = super.render();
+        return parent;
+    }
+
+    public void toIngame() {
+        hybridController.get().openStackpane("ingame");
+    }
+}
