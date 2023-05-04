ef82461d4add59bb2a415fb0a4f540abcd195909
Alexander Haar <uk077258@student.uni-kassel.de>
2023-05-04 18:02:17 +0200

    feat: reduced one function

--- src/main/java/de/uniks/stpmon/k/controller/LobbyController.java
+++ src/main/java/de/uniks/stpmon/k/controller/LobbyController.java
@@ -27,6 +27,6 @@ public class LobbyController extends Controller {
     }
 
     public void toIngame() {
-        hybridController.get().openStackpane("ingame");
+        hybridController.get().openSidebar("ingame");
     }
 }
