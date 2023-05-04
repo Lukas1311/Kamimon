0ef775724bee1a32f6212b4b6f7a3ea75cc83c97
Alexander Haar <uk077258@student.uni-kassel.de>
2023-05-04 17:35:56 +0200

    feat: init Pause.fxml and PauseController

---
+++ src/main/java/de/uniks/stpmon/k/controller/PauseController.java
@@ -0,0 +1,12 @@
+package de.uniks.stpmon.k.controller;
+
+import javafx.scene.Parent;
+
+public class PauseController extends Controller {
+
+    @Override
+    public Parent render() {
+        final Parent parent = super.render();
+        return parent;
+    }
+}
