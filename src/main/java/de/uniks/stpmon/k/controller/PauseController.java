0cfc733abb184d166fed3f94e71d11a11637dfdf
Alexander Haar <uk077258@student.uni-kassel.de>
2023-05-04 18:01:32 +0200

    feat: inject PauseController

--- src/main/java/de/uniks/stpmon/k/controller/PauseController.java
+++ src/main/java/de/uniks/stpmon/k/controller/PauseController.java
@@ -2,7 +2,13 @@ package de.uniks.stpmon.k.controller;
 
 import javafx.scene.Parent;
 
+import javax.inject.Inject;
+
 public class PauseController extends Controller {
+
+    @Inject
+    public PauseController() {
+    }
 
     @Override
     public Parent render() {
