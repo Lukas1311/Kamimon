6a8821b37eafb1e310c9578bfff7e22213338ed5
sh0xy <53441989+sh0xy@users.noreply.github.com>
2023-05-04 13:16:53 +0200

    feat: add preferences + add isRememberMe()

--- src/main/java/de/uniks/stpmon/k/service/AuthenticationService.java
+++ src/main/java/de/uniks/stpmon/k/service/AuthenticationService.java
@@ -1,5 +1,7 @@
 package de.uniks.stpmon.k.service;
 
+import java.util.prefs.Preferences;
+
 import javax.inject.Inject;
 
 import de.uniks.stpmon.k.dto.*;
@@ -11,21 +13,25 @@ public class AuthenticationService  {
     private final TokenStorage tokenStorage;
     private final AuthenticationApiService authApiService;
     private final UserStorage userStorage;
+    private final Preferences preferences;
     // TODO: add preferences
 
     @Inject
-    public AuthenticationService(TokenStorage tokenStorage, AuthenticationApiService authApiService, UserStorage userStorage) {
+    public AuthenticationService(TokenStorage tokenStorage, AuthenticationApiService authApiService, UserStorage userStorage, Preferences preferences) {
         this.tokenStorage = tokenStorage;
         this.authApiService = authApiService;
         this.userStorage = userStorage;
+        this.preferences = preferences;
     }
 
-    public Observable<LoginResult> login(String username, String password) {
+    public Observable<LoginResult> login(String username, String password, boolean rememberMe) {
         return authApiService.login(new LoginDto(username, password)).map(lr -> {
             tokenStorage.setToken(lr.accessToken());
+            if (rememberMe) {
+                preferences.put("refreshToken", lr.refreshToken());
+            }
             //Add User to UserStorage
-            User user = new User(lr._id(), lr.name(), lr.status(), lr.avatar(), lr.friends());
-            userStorage.setUser(user);
+            userStorage.setUser(new User(lr._id(), lr.name(), lr.status(), lr.avatar(), lr.friends()));
             return lr;
         });
     }
@@ -38,6 +44,9 @@ public class AuthenticationService  {
     }
 
     // TODO: implement isRememberMe
+    public boolean isRememberMe() {
+        return false;
+    }
 
     public Observable<LoginResult> refresh() {
         // TODO: add refresh token from preferences
