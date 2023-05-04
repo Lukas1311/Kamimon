022fe5f06a82d322acf62ccaf58e5c55e9d4f079
Alexander Haar <uk077258@student.uni-kassel.de>
2023-05-04 18:57:15 +0200

    feat: init pause

--- src/main/java/de/uniks/stpmon/k/controller/PauseController.java
+++ src/main/java/de/uniks/stpmon/k/controller/PauseController.java
@@ -1,10 +1,15 @@
 package de.uniks.stpmon.k.controller;
 
+import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.layout.Pane;
 
 import javax.inject.Inject;
 
 public class PauseController extends Controller {
+
+    @FXML
+    public Pane pause;
 
     @Inject
     public PauseController() {
