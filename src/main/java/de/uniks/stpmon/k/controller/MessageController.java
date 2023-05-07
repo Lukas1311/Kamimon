48f4ee1b8656572ad6ac9b90dc5227eaad61346e
sh0xy <53441989+sh0xy@users.noreply.github.com>
2023-05-07 16:30:10 +0200

    feat: add message

--- src/main/java/de/uniks/stpmon/k/controller/MessageController.java
+++ src/main/java/de/uniks/stpmon/k/controller/MessageController.java
@@ -1,5 +1,6 @@
 package de.uniks.stpmon.k.controller;
 
+import de.uniks.stpmon.k.dto.Message;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.layout.HBox;
@@ -16,19 +17,21 @@ public class MessageController extends Controller {
     @FXML
     public Text sendTime;
 
-    public MessageController() {
+    private Message message;
 
+    public MessageController(Message msg) {
+        this.message = msg;
     }
 
     @Override
     public Parent render() {
         final Parent parent = super.render();
 
-        bodyText.setText("Testnachricht");
+        // bodyText.setText("Testnachricht");
 
         //TODO: implement method when Message is ready
-//        senderName.setText(message.sender());
-//        bodyText.setText(message.body());
+        senderName.setText(message.sender());
+        bodyText.setText(message.body());
 
         return parent;
     }
