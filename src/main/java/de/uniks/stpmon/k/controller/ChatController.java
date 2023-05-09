c3c5b5c19be43d6e5f45c41d9e0c27d42cf34696
sh0xy <53441989+sh0xy@users.noreply.github.com>
2023-05-09 03:01:00 +0200

    feat: add get chat messages on initial load

--- src/main/java/de/uniks/stpmon/k/controller/ChatController.java
+++ src/main/java/de/uniks/stpmon/k/controller/ChatController.java
@@ -1,9 +1,14 @@
 package de.uniks.stpmon.k.controller;
 
+import de.uniks.stpmon.k.dto.Group;
 import de.uniks.stpmon.k.dto.Message;
+import de.uniks.stpmon.k.rest.GroupApiService;
+import de.uniks.stpmon.k.rest.MessageApiService;
+import de.uniks.stpmon.k.service.GroupService;
 import de.uniks.stpmon.k.service.MessageService;
 import de.uniks.stpmon.k.service.RegionService;
 import de.uniks.stpmon.k.service.UserStorage;
+import de.uniks.stpmon.k.views.MessageCell;
 import javafx.beans.property.ListProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleStringProperty;
@@ -14,6 +19,7 @@ import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -38,46 +44,62 @@ public class ChatController extends Controller {
     @Inject
     RegionService regionService;
     @Inject
+    GroupApiService groupApiService;
+    @Inject
     UserStorage userStorage;
 
     private StringProperty regionName;
     private StringProperty functionStatus;
-    private ObservableList<Message> messageList = FXCollections.observableArrayList();
+    private ObservableList<Message> messages = FXCollections.observableArrayList();
 
     @Inject
     public ChatController() {
-
     }
 
+    @Override
+    public void init() { // get all messages in one chat
+        // TODO: get the chat = group_id from somewhere
+        System.out.println("this should be a group id: " + groupApiService.getGroups().firstElement().blockingGet().get(0)._id());
+        disposables.add(msgService
+            .getAllMessages(
+                // TODO: remove only for testing a group id
+                "groups",
+                "6457a3ce4d233ed4626d20c0"
+                // groupService.getOwnGroups().firstElement().blockingGet().get(0)._id()
+            ).observeOn(FX_SCHEDULER)
+            .subscribe(messages -> {
+                this.messages.setAll(messages);
+            }, error -> {
+                System.out.println("look here for the error: " + error);
+            })
+        );
+    }
 
     @Override
     public Parent render() {
         final Parent parent = super.render();
 
-        // binds the messageBoxList to the messageArea
-        ListProperty<Message> messageProperty = new SimpleListProperty<>(FXCollections.observableArrayList());
-        // messageProperty.bindBidirectional(messageList);
-        // messageArea.getChildren().addAll(messageList);
-
-        //TODO
-
-        messageArea.getChildren().add(new MessageController(new Message(null, null, null, "nobody", "Testnachricht")).render());
-        messageArea.getChildren().add(new MessageController(new Message(null, null, null, "jemand", "Testnachricht2")).render());
-        
-
         addRegionsToChoiceBox();
 
-        // clear message field on button click
-        sendButton.setOnAction(click -> messageField.clear());
+        final ListView<Message> messages = new ListView<>(this.messages);
+        messages.setCellFactory(param -> new MessageCell());
+
+        // TODO: edit and delete single messages by chosing them and some listener stuff
+        // messages.getSelectionModel().selectedItemProperty()... listener stuff
+
         // disable button if field empty
         sendButton.disableProperty().bind(messageField.textProperty().isEmpty());
-
-
+        sendButton.setOnAction(click -> {
+            // clear message field and send on button click
+            messageField.clear();
+            sendMessage();
+        });
 
         regionName = new SimpleStringProperty("");
         // bind regionName to selected choice box item
         regionName.bind(regionPicker.getSelectionModel().selectedItemProperty());
-        
+        messageArea.getChildren().setAll(messages);
+
         return parent;
     }
 
@@ -95,18 +117,14 @@ public class ChatController extends Controller {
     public void sendMessage() {
         System.out.println("msg: " + messageField.getText() + ",region: " +  regionName.get());
         // TODO: remove afterwards (non-functional: visual testing only)
-        messageArea.getChildren().add(new MessageController(new Message(null, null, null, userStorage.getUser().name(), messageField.getText())).render());
-        //TODO: find out what the correct regions strings are
-        //TODO: find out a proper message ID as the parent
+        // messageArea.getChildren().add(new MessageController(new Message(null, null, null, userStorage.getUser().name(), messageField.getText())).render());
         disposables.add(msgService
-            .sendMessage(messageField.getText(), regionName.get(), "123")
+            .sendMessage(messageField.getText(), "groups", "6457a3ce4d233ed4626d20c0")
             .observeOn(FX_SCHEDULER)
             .subscribe(msg -> {
                 functionStatus.set("Message sent");
                 System.out.println(msg);
-                messageArea.getChildren().add(
-                    new MessageController(msg).render()
-                );
+                messageArea.getChildren().add(new MessageCell());
             }, error -> {
                 // TODO: this still receives a HTTP 400 and I don't know why
                 System.out.println("look here for the error: " + error);
