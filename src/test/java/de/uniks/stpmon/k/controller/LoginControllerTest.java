c2dcbd3fe72ae6ab3daded99d64493ab8968d3de
sh0xy <53441989+sh0xy@users.noreply.github.com>
2023-05-09 20:56:40 +0200

    fix: add registerTest + add more asserts to test buttons

--- src/test/java/de/uniks/stpmon/k/controller/LoginControllerTest.java
+++ src/test/java/de/uniks/stpmon/k/controller/LoginControllerTest.java
@@ -13,18 +13,24 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.matcher.control.LabeledMatchers;
+import static org.testfx.assertions.api.Assertions.assertThat;
+import static org.testfx.api.FxAssert.verifyThat;
 
 import de.uniks.stpmon.k.App;
 import de.uniks.stpmon.k.dto.LoginResult;
+import de.uniks.stpmon.k.dto.User;
 import de.uniks.stpmon.k.service.AuthenticationService;
 import de.uniks.stpmon.k.service.NetworkAvailability;
 import de.uniks.stpmon.k.service.TokenStorage;
 import de.uniks.stpmon.k.service.UserService;
 
-import io.reactivex.rxjava3.core.Observable;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javax.inject.Provider;
+import io.reactivex.rxjava3.core.Observable;
 
 
 
@@ -74,14 +80,42 @@ public class LoginControllerTest extends ApplicationTest {
         // TODO: make sure to adjust count of tabs when Login fxml is changed
         // tab 3 times to go on the login button -> is faster than click on button (no mouse movement)
         write("\t\t\t");
+        Button selectedButton = lookup("#loginButton").queryAs(Button.class);
+        assertThat(selectedButton).isFocused();
         press(KeyCode.ENTER);
         release(KeyCode.ENTER);
+
+        Label label = lookup("#errorLabel").queryAs(Label.class);
+        verifyThat(label, LabeledMatchers.hasText("Login successful"));
 
         verify(app).show(mock);
     }
 
     @Test
     void testRegister() {
+        User bob = new User(null, "Bob", null, null, null);
+        when(userService.addUser(any(), any())).thenReturn(Observable.just(
+            bob
+        ));
 
+        // tab into username input field
+        write("\t");
+        // type username and tab into password field
+        write("Bob\t");
+        // type password
+        write("password");
+        // TODO: make sure to adjust count of tabs when Login fxml is changed
+        // tab 3 times to go on the login button -> is faster than click on button (no mouse movement)
+        write("\t\t\t\t");
+        // Retrieve the currently selected button
+        Button selectedButton = lookup("#registerButton").queryAs(Button.class);
+        assertThat(selectedButton).isFocused();
+        press(KeyCode.ENTER);
+        release(KeyCode.ENTER);
+
+        Label label = lookup("#errorLabel").queryAs(Label.class);
+        verifyThat(label, LabeledMatchers.hasText("Registration successful"));
+        // app stays in login controller after registration call, only afterwards it will login into hybridContoller
+        verify(app).show(loginController);
     }
 }
