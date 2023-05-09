52b35a1903c388264207d47cb1450cb77174088a
halber <XhalberX@gmail.com>
2023-05-09 13:28:59 +0200

    init ChatEntryController

--- src/main/java/de/uniks/stpmon/k/controller/ChatEntryController.java
+++ src/main/java/de/uniks/stpmon/k/controller/ChatEntryController.java
@@ -1,4 +1,29 @@
 package de.uniks.stpmon.k.controller;
 
-public class ChatEntryController {
+import de.uniks.stpmon.k.service.GroupService;
+import javafx.scene.Parent;
+import javafx.scene.layout.VBox;
+
+import javax.inject.Inject;
+
+public class ChatEntryController  extends Controller {
+
+    @Inject
+    public ChatEntryController() {
+        }
+
+        @Override
+        public void init() {
+        }
+
+        @Override
+        public void destroy() {
+            super.destroy();
+        }
+
+        @Override
+        public Parent render() {
+            final Parent parent = super.render();
+            return parent;
+        }
 }
