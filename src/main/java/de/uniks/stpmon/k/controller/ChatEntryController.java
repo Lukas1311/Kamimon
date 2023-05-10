7d107eb3cb833819f65f78c421db03f4c66f2abb
sh0xy <53441989+sh0xy@users.noreply.github.com>
2023-05-10 23:44:42 +0200

    feat: add chatListController + group field, add method to open a chat

--- src/main/java/de/uniks/stpmon/k/controller/ChatEntryController.java
+++ src/main/java/de/uniks/stpmon/k/controller/ChatEntryController.java
@@ -1,5 +1,6 @@
 package de.uniks.stpmon.k.controller;
 
+import de.uniks.stpmon.k.dto.Group;
 import de.uniks.stpmon.k.service.GroupService;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
@@ -8,6 +9,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 import javax.inject.Inject;
+import javax.inject.Provider;
 import javax.inject.Singleton;
 
 public class ChatEntryController  extends Controller {
@@ -17,29 +19,39 @@ public class ChatEntryController  extends Controller {
     @FXML
     public VBox chatEntry;
     @Inject
-    @Singleton
-    ChatListController chatListController;
+    ChatController chatController;
 
+    private final ChatListController chatListController;
+    private final Group group;
     private final String name;
+
     @Inject
-    public ChatEntryController(String name) {
-        this.name = name;
-        }
+    public ChatEntryController(Group group, ChatListController chatListController) {
+        this.group = group;
+        this.name = group.name();
+        this.chatListController = chatListController;
+    }
 
-        @Override
+    @Override
         public void init() {
-        }
+    }
 
-        @Override
-        public void destroy() {
-            super.destroy();
-        }
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
 
-        @Override
-        public Parent render() {
-            final Parent parent = super.render();
-            chatName.setText(name);
-            return parent;
-        }
+    @Override
+    public Parent render() {
+        final Parent parent = super.render();
+        chatName.setText(name);
+        parent.setOnMouseClicked(e -> openChat());
+
+        return parent;
+    }
+
+    public void openChat() {
+        chatListController.openChat(group);
+    }
 
 }
