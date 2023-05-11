4c28451926d5e9d5f3ee5e2bac28872e04923ca2
sh0xy <53441989+sh0xy@users.noreply.github.com>
2023-05-11 03:02:36 +0200

    feat: rework message cell

--- src/main/java/de/uniks/stpmon/k/views/MessageCell.java
+++ src/main/java/de/uniks/stpmon/k/views/MessageCell.java
@@ -1,15 +1,36 @@
 package de.uniks.stpmon.k.views;
 
-import javax.inject.Provider;
+import java.io.IOException;
 
-import de.uniks.stpmon.k.controller.ChatController;
 import de.uniks.stpmon.k.controller.MessageController;
 import de.uniks.stpmon.k.dto.Message;
+import de.uniks.stpmon.k.dto.User;
+import de.uniks.stpmon.k.service.UserStorage;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.control.ListCell;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 public class MessageCell extends ListCell<Message> {
 
-    public MessageCell() {
+    @FXML
+    public HBox textBox;
+    @FXML
+    public Text bodyText;
+    @FXML
+    public Text senderName;
+    @FXML
+    public Text sendTime;
+
+
+    private final User user;
+
+    public MessageCell(User user) {
+        this.user = user;
     }
 
     @Override
@@ -19,8 +40,26 @@ public class MessageCell extends ListCell<Message> {
             setGraphic(null);
             setText(null);
         } else {
-            final MessageController messageController = new MessageController(item);
-            setGraphic(messageController.render());
+            // final MessageController messageController = new MessageController(item);
+            Text senderName = new Text(isOwnMessage(item) ? user.name() : item.sender());
+            senderName.setFont(new Font(10.0));
+            Text sendTime = new Text(item.createdAt());
+            sendTime.setFont(new Font(10.0));
+            Text bodyText = new Text(item.body());
+            HBox textBox = new HBox(bodyText);
+            VBox senderBox = new VBox(senderName);
+            VBox timeBox = new VBox(sendTime);
+            HBox senderAndTimeBox = new HBox(senderBox, timeBox);
+            VBox messageBox = new VBox(textBox, senderAndTimeBox);
+            
+            // Set the alignment of the text based on the item's alignment property
+            messageBox.setAlignment(isOwnMessage(item) ? Pos.CENTER_RIGHT : Pos.CENTER_LEFT);
+            setGraphic(messageBox);
+            // setGraphic(messageController.render());
         }
+    }
+
+    private boolean isOwnMessage(Message msg) {
+        return msg.sender().equals(user._id());
     }
 }
