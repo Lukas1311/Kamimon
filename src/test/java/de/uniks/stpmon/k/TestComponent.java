a9de0efdc62fba5a7f7e9500cddafea8e87ea720
Alexander Haar <uk077258@student.uni-kassel.de>
2023-05-12 19:22:53 +0200

    adding HybridController to TestComponent -> test is similiar to appTest

--- src/test/java/de/uniks/stpmon/k/TestComponent.java
+++ src/test/java/de/uniks/stpmon/k/TestComponent.java
@@ -1,14 +1,19 @@
 package de.uniks.stpmon.k;
 
 import dagger.Component;
+import de.uniks.stpmon.k.controller.HybridController;
 
 import javax.inject.Singleton;
 
 @Component(modules = {TestModule.class, GroupTestModule.class, MessageTestModule.class})
 @Singleton
 public interface TestComponent extends MainComponent {
+
+    HybridController hybridController();
     @Component.Builder
     interface Builder extends MainComponent.Builder {
+
+        @Override
         TestComponent build();
     }
 }
