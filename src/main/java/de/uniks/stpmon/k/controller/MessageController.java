14501d8187e23d162c2aba50346618b116d570a9
sh0xy <53441989+sh0xy@users.noreply.github.com>
2023-05-12 02:42:54 +0200

    refactor: move to messageController (easier)

--- src/main/java/de/uniks/stpmon/k/controller/MessageController.java
+++ src/main/java/de/uniks/stpmon/k/controller/MessageController.java
@@ -1,13 +1,24 @@
 package de.uniks.stpmon.k.controller;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 import de.uniks.stpmon.k.dto.Message;
+import de.uniks.stpmon.k.dto.User;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 public class MessageController extends Controller {
 
+    @FXML
+    public VBox messageBox;
     @FXML
     public HBox textBox;
     @FXML
@@ -18,21 +29,47 @@ public class MessageController extends Controller {
     public Text sendTime;
 
     private Message message;
+    private boolean isOwnMessage;
+    private User me;
 
-    public MessageController(Message msg) {
+    public MessageController(Message msg, boolean isOwn, User user) {
         this.message = msg;
+        this.isOwnMessage = isOwn;
+        this.me = user;
     }
 
     @Override
     public Parent render() {
         final Parent parent = super.render();
 
+        System.out.println(me._id());
+
         // bodyText.setText("Testnachricht");
 
-        //TODO: implement method when Message is ready
-        senderName.setText(message.sender());
+        //TODO: implement method that gets other users name e.g. with friends list
+        senderName.setText(isOwnMessage ? me.name() : message.sender());
         bodyText.setText(message.body());
+        sendTime.setText(convertDateTimeToTime(message.createdAt()));
+        // Set the alignment of the text based on the item's alignment property
+        messageBox.setAlignment(isOwnMessage ? Pos.CENTER_RIGHT : Pos.CENTER_LEFT);
 
         return parent;
+    }
+
+    private String convertDateTimeToTime(String dateTimeString) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        LocalDateTime dateTime = LocalDateTime.parse(dateTimeString, formatter);
+
+        ZoneId sourceZone = ZoneId.of("UTC"); // assuming the given time is in UTC
+        ZoneId germanyZone = ZoneId.of("Europe/Berlin");
+
+        ZonedDateTime sourceDateTime = ZonedDateTime.of(dateTime, sourceZone);
+        ZonedDateTime germanyDateTime = sourceDateTime.withZoneSameInstant(germanyZone);
+
+        LocalTime germanTime = germanyDateTime.toLocalTime();
+        // strips the seconds and milliseconds from time object
+        LocalTime strippedTime = germanTime.truncatedTo(java.time.temporal.ChronoUnit.MINUTES);
+
+        return strippedTime.toString();
     }
 }
--- src/main/java/de/uniks/stpmon/k/views/MessageCell.java
+++ src/main/java/de/uniks/stpmon/k/views/MessageCell.java
@@ -2,6 +2,14 @@ package de.uniks.stpmon.k.views;
 
 import java.io.IOException;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+
 import de.uniks.stpmon.k.controller.MessageController;
 import de.uniks.stpmon.k.dto.Message;
 import de.uniks.stpmon.k.dto.User;
@@ -10,8 +18,15 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.control.ListCell;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
@@ -40,26 +55,61 @@ public class MessageCell extends ListCell<Message> {
             setGraphic(null);
             setText(null);
         } else {
-            // final MessageController messageController = new MessageController(item);
-            Text senderName = new Text(isOwnMessage(item) ? user.name() : item.sender());
-            senderName.setFont(new Font(10.0));
-            Text sendTime = new Text(item.createdAt());
-            sendTime.setFont(new Font(10.0));
-            Text bodyText = new Text(item.body());
-            HBox textBox = new HBox(bodyText);
-            VBox senderBox = new VBox(senderName);
-            VBox timeBox = new VBox(sendTime);
-            HBox senderAndTimeBox = new HBox(senderBox, timeBox);
-            VBox messageBox = new VBox(textBox, senderAndTimeBox);
             
-            // Set the alignment of the text based on the item's alignment property
-            messageBox.setAlignment(isOwnMessage(item) ? Pos.CENTER_RIGHT : Pos.CENTER_LEFT);
-            setGraphic(messageBox);
-            // setGraphic(messageController.render());
+            // Text senderName = new Text(isOwnMessage(item) ? user.name() : item.sender());
+            // senderName.setFont(new Font(10.0));
+            // // set the time
+            // Text sendTime = new Text(
+            //     convertDateTimeToTime(item.createdAt())
+            // );
+            // sendTime.setFont(new Font(10.0));
+            // Text bodyText = new Text(item.body());
+
+            // // model the text box where the message body text is inside
+            // HBox textBox = new HBox(bodyText);
+            // textBox.setBorder(new Border(new BorderStroke(
+            //     Color.BLACK,
+            //     BorderStrokeStyle.SOLID,
+            //     new CornerRadii(5),
+            //     new BorderWidths(1)
+            // )));
+            // textBox.setPrefWidth(Region.USE_COMPUTED_SIZE); // wrap box around the text
+
+            // VBox senderBox = new VBox(senderName);
+            // senderBox.setAlignment(Pos.TOP_LEFT);
+            // VBox timeBox = new VBox(sendTime);
+            // timeBox.setAlignment(Pos.TOP_RIGHT);
+            // HBox senderAndTimeBox = new HBox(senderBox, timeBox);
+            // VBox messageBox = new VBox(textBox, senderAndTimeBox);
+            
+            // // Set the alignment of the text based on the item's alignment property
+            // messageBox.setAlignment(isOwnMessage(item) ? Pos.CENTER_RIGHT : Pos.CENTER_LEFT);
+            // setGraphic(messageBox);
+
+            // TODO: create everything directly here (like above) or like this as alternative
+            final MessageController messageController = new MessageController(item, isOwnMessage(item), user);
+            setGraphic(messageController.render());
         }
     }
 
     private boolean isOwnMessage(Message msg) {
         return msg.sender().equals(user._id());
     }
+
+    // private String convertDateTimeToTime(String dateTimeString) {
+    //     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+    //     LocalDateTime dateTime = LocalDateTime.parse(dateTimeString, formatter);
+
+    //     ZoneId sourceZone = ZoneId.of("UTC"); // assuming the given time is in UTC
+    //     ZoneId germanyZone = ZoneId.of("Europe/Berlin");
+
+    //     ZonedDateTime sourceDateTime = ZonedDateTime.of(dateTime, sourceZone);
+    //     ZonedDateTime germanyDateTime = sourceDateTime.withZoneSameInstant(germanyZone);
+
+    //     LocalTime germanTime = germanyDateTime.toLocalTime();
+    //     // strips the seconds and milliseconds from time object
+    //     LocalTime strippedTime = germanTime.truncatedTo(java.time.temporal.ChronoUnit.MINUTES);
+
+    //     return strippedTime.toString();
+    // }
 }
