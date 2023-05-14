3defde34bc05d3d32bcb42ce41b7182816435bdd
halber <XhalberX@gmail.com>
2023-05-14 15:35:55 +0200

    add: openFriends Test and setting user for UserStorage

--- src/test/java/de/uniks/stpmon/k/TestComponent.java
+++ src/test/java/de/uniks/stpmon/k/TestComponent.java
@@ -2,6 +2,7 @@ package de.uniks.stpmon.k;
 
 import dagger.Component;
 import de.uniks.stpmon.k.controller.HybridController;
+import de.uniks.stpmon.k.service.UserStorage;
 
 import javax.inject.Singleton;
 
@@ -10,6 +11,8 @@ import javax.inject.Singleton;
 public interface TestComponent extends MainComponent {
 
     HybridController hybridController();
+
+    UserStorage userStorage();
     @Component.Builder
     interface Builder extends MainComponent.Builder {
 
--- src/test/java/de/uniks/stpmon/k/controller/HybridControllerTest.java
+++ src/test/java/de/uniks/stpmon/k/controller/HybridControllerTest.java
@@ -3,6 +3,8 @@ package de.uniks.stpmon.k.controller;
 import de.uniks.stpmon.k.App;
 import de.uniks.stpmon.k.DaggerTestComponent;
 import de.uniks.stpmon.k.TestComponent;
+import de.uniks.stpmon.k.dto.User;
+import de.uniks.stpmon.k.service.UserStorage;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -10,6 +12,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.testfx.framework.junit5.ApplicationTest;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -19,11 +23,11 @@ class HybridControllerTest extends ApplicationTest {
     private final App app = new App(null);
     private final TestComponent component = (TestComponent) DaggerTestComponent.builder().mainApp(app).build();
     private final HybridController hybridController = component.hybridController();
+    UserStorage userStorage = component.userStorage();
 
 
     @Override
     public void start(Stage stage) throws Exception {
-
         app.start(stage);
         app.show(hybridController);
         stage.requestFocus();
@@ -34,6 +38,15 @@ class HybridControllerTest extends ApplicationTest {
     public void openChat() {
         type(KeyCode.ENTER);
         VBox chatList = lookup("#chatList").query();
+        assertNotNull(chatList);
+    }
+
+    @Test
+    public void openFriends() {
+        userStorage.setUser(new User("1", "Bob", "", "", new ArrayList<>()));
+        write("\t");
+        type(KeyCode.ENTER);
+        VBox chatList = lookup("#friendList").query();
         assertNotNull(chatList);
     }
 }
