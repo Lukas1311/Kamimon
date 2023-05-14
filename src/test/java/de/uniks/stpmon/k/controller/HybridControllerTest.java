5fe4aa43d5476b99e4ebbbecc4ca1617ff2432a3
halber <XhalberX@gmail.com>
2023-05-14 15:32:30 +0200

    add: openChat test

--- src/test/java/de/uniks/stpmon/k/controller/HybridControllerTest.java
+++ src/test/java/de/uniks/stpmon/k/controller/HybridControllerTest.java
@@ -3,15 +3,15 @@ package de.uniks.stpmon.k.controller;
 import de.uniks.stpmon.k.App;
 import de.uniks.stpmon.k.DaggerTestComponent;
 import de.uniks.stpmon.k.TestComponent;
-import javafx.application.Platform;
-import javafx.scene.layout.StackPane;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.testfx.framework.junit5.ApplicationTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(MockitoExtension.class)
 class HybridControllerTest extends ApplicationTest {
@@ -23,6 +23,7 @@ class HybridControllerTest extends ApplicationTest {
 
     @Override
     public void start(Stage stage) throws Exception {
+
         app.start(stage);
         app.show(hybridController);
         stage.requestFocus();
@@ -30,37 +31,9 @@ class HybridControllerTest extends ApplicationTest {
 
 
     @Test
-    public void testOpenSidebar() {
-        StackPane stackPane = lookup("#stackPane").query();
-        assertEquals(hybridController.stackPane.getId(), stackPane.getId());
-        Platform.runLater(() -> {
-            // open ChatList, check if ID are equal
-            hybridController.sidebarController.get().openChat();
-            assertEquals("chatList", hybridController.stackPane.getChildren().get(1).getId());
-            /*
-            // open FriendList, check if ID are equal
-            hybridController.openSidebar("friends");
-            assertEquals("friendList", hybridController.stackPane.getChildren().get(1).getId());
-             */
-
-            // open Pause, check if ID are equal
-            hybridController.sidebarController.get().toPause();
-            assertEquals("pause", hybridController.stackPane.getChildren().get(0).getId());
-
-            // clicking again Pause to go back to ingame, check if ID are equal
-            hybridController.sidebarController.get().toPause();
-            assertEquals("ingame", hybridController.stackPane.getChildren().get(0).getId());
-
-            // Lobby, check if ID are equal
-            hybridController.sidebarController.get().backtoLobby();
-            assertEquals("pane", hybridController.stackPane.getChildren().get(0).getId());
-
-            // Ingame, check if ID are equal
-            hybridController.openSidebar("ingame");
-            assertEquals("ingame", hybridController.stackPane.getChildren().get(0).getId());
-
-            // Logout, check if app shows login
-            hybridController.sidebarController.get().logout();
-        });
+    public void openChat() {
+        type(KeyCode.ENTER);
+        VBox chatList = lookup("#chatList").query();
+        assertNotNull(chatList);
     }
 }
