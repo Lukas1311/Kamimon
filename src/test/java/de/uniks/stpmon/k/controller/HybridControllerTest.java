11bafe1cea4bf9b5ce993af48606282303294bd7
halber <XhalberX@gmail.com>
2023-05-15 16:03:48 +0200

    add: enter button to chatCell changed clickOn to navigate with tabs

--- src/test/java/de/uniks/stpmon/k/controller/HybridControllerTest.java
+++ src/test/java/de/uniks/stpmon/k/controller/HybridControllerTest.java
@@ -55,11 +55,12 @@ class HybridControllerTest extends ApplicationTest {
         VBox chatList = lookup("#chatList").query();
         assertNotNull(chatList);
 
+        userStorage.setUser(new User("1", "Bob", "", "", new ArrayList<>()));
         when(eventListener.<Message>listen(any(), any())).thenReturn(Observable.empty());
         // pressing on a chat and check if chatScreen is shown
-        clickOn("#TestGroup0");
+        write("\t\t\t\t\t");
+        press(KeyCode.ENTER).release(KeyCode.ENTER);
         waitForFxEvents();
-        printSceneGraph();
         VBox chatScreen = lookup("#chatScreen").query();
         assertNotNull(chatScreen);
     }
@@ -123,24 +124,5 @@ class HybridControllerTest extends ApplicationTest {
         waitForFxEvents();
         Pane pane = lookup("#loginScreen").query();
         assertNotNull(pane);
-    }
-
-
-    public void printSceneGraph() {
-        printNode(app.getStage().getScene().getRoot(), 0);
-    }
-
-    private void printNode(javafx.scene.Node node, int depth) {
-        for (int i = 0; i < depth; i++) {
-            System.out.print("  "); // Print indentation
-        }
-        System.out.println(node);
-
-        if (node instanceof javafx.scene.Parent) {
-            javafx.scene.Parent parent = (javafx.scene.Parent) node;
-            for (javafx.scene.Node child : parent.getChildrenUnmodifiable()) {
-                printNode(child, depth + 1);
-            }
-        }
     }
 }
