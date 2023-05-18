b1c07e96b1c115d84d86bc6a7446483a57c4225c
IsmailHalli <uk083810@student.uni-kassel.de>
2023-05-18 19:45:18 +0200

    feat: Add IntroductionController

---
+++ src/main/java/de/uniks/stpmon/k/controller/IntroductionController.java
@@ -0,0 +1,25 @@
+package de.uniks.stpmon.k.controller;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import javax.inject.Inject;
+import java.util.Objects;
+
+public class IntroductionController extends Controller{
+
+
+
+    @Inject
+    public IntroductionController(){
+
+    }
+
+    @Override
+    public Parent render() {
+        return null;
+    }
+}
