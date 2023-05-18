a30aa3bedd72b98aa58b728d272ca59bd38278b0
IsmailHalli <uk083810@student.uni-kassel.de>
2023-05-18 20:19:05 +0200

    feat: Add back funtion

--- src/main/java/de/uniks/stpmon/k/controller/IntroductionController.java
+++ src/main/java/de/uniks/stpmon/k/controller/IntroductionController.java
@@ -22,6 +22,8 @@ public class IntroductionController extends Controller{
     public ImageView imageIntroduction;
     @FXML
     public Button further;
+    @FXML
+    public Button back;
     @Inject
     Provider<HybridController> hybridControllerProvider;
 
@@ -35,23 +37,47 @@ public class IntroductionController extends Controller{
         final Parent parent = super.render();
         final Image imageOne = loadImage("introductionSheetOne.png");
         imageIntroduction.setImage(imageOne);
+        if (imageIntroduction.getImage() == imageOne) {
+            back.setVisible(false);
+        }
         return parent;
     }
 
     public void nextSheet(ActionEvent event) {
+        back.setVisible(true);
         final Image imageTwo = loadImage("introductionSheetTwo.png");
+        final Image imageThree = loadImage("introductionSheetThree.png");
         imageIntroduction.setImage(imageTwo);
         further.setOnAction(event1 -> {
-            final Image imageThree = loadImage("introductionSheetThree.png");
             imageIntroduction.setImage(imageThree);
             further.setOnAction(event2 -> {
                 app.show(hybridControllerProvider.get());
             });
-            //app.show(hybridControllerProvider.get());
         });
     }
 
     private Image loadImage(String image) {
         return new Image(Objects.requireNonNull(LoadingScreenController.class.getResource(image)).toString());
+    }
+
+    public void previousSheet(ActionEvent event) {
+        final Image previousImageOne = loadImage("introductionSheetOne.png");
+        final Image previousImageTwo = loadImage("introductionSheetTwo.png");
+        imageIntroduction.setImage(previousImageTwo);
+        back.setOnAction(event1 -> {
+            imageIntroduction.setImage(previousImageOne);
+        });
+        /*Image currentImage = imageIntroduction.getImage();
+        final Image previousImageOne = loadImage("introductionSheetOne.png");
+        final Image previousImageTwo = loadImage("introductionSheetTwo.png");
+        final Image previousImageThree = loadImage("introductionSheetThree.png");
+        if(Objects.equals(currentImage, previousImageTwo)) {
+            imageIntroduction.setImage(previousImageOne);
+            back.setVisible(false);
+        } else if(Objects.equals(currentImage, previousImageThree)) {
+            imageIntroduction.setImage(previousImageTwo);
+        }
+
+         */
     }
 }
