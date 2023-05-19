61864b9c38abe26d5c8d52b38e6c31e033cedaa6
discobreak21 <bastianbaer@hotmail.com>
2023-05-19 15:44:56 +0200

    feat: introduction update

--- src/main/java/de/uniks/stpmon/k/controller/LoginController.java
+++ src/main/java/de/uniks/stpmon/k/controller/LoginController.java
@@ -13,6 +13,8 @@ import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -169,7 +171,6 @@ public class LoginController extends Controller {
                     loginWithCredentials(user.name(), passwordInput.getText(), rememberMe.isSelected(), false);
                 }, error -> {
                     errorText.set(getErrorMessage(error));
-                    System.out.println("look here for the error: " + error);
                 }));
     }
 
