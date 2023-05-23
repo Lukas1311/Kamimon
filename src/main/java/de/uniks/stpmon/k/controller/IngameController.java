2d72bba8129405ef2c98f74ca721f577829302aa
halber <XhalberX@gmail.com>
2023-05-23 21:05:47 +0200

    add: closeSidebar if user clicks on ingame screen

--- src/main/java/de/uniks/stpmon/k/controller/IngameController.java
+++ src/main/java/de/uniks/stpmon/k/controller/IngameController.java
@@ -1,22 +1,34 @@
 package de.uniks.stpmon.k.controller;
 
-import de.uniks.stpmon.k.App;
+import de.uniks.stpmon.k.controller.sidebar.HybridController;
+import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
 
 import javax.inject.Inject;
+import javax.inject.Provider;
 import javax.inject.Singleton;
 
 @Singleton
-public class IngameController extends Controller{
+public class IngameController extends Controller {
 
+    @FXML
+    public BorderPane ingame;
+
+    @Inject
+    Provider<HybridController> hybridControllerProvider;
 
     @Inject
     public IngameController() {
     }
 
     @Override
-    public Parent render(){
+    public Parent render() {
         final Parent parent = super.render();
         return parent;
+    }
+
+    public void closeSidebar() {
+        hybridControllerProvider.get().closeTab();
     }
 }
