9188be2aa342ab2eae7724dab94f9609aed6ac46
discobreak21 <bastianbaer@hotmail.com>
2023-05-23 12:32:58 +0200

    feat: mock methods of PresetsApiModule

--- src/main/java/de/uniks/stpmon/k/rest/PresetApiService.java
+++ src/main/java/de/uniks/stpmon/k/rest/PresetApiService.java
@@ -3,22 +3,25 @@ package de.uniks.stpmon.k.rest;
 import de.uniks.stpmon.k.dto.AbilityDto;
 import de.uniks.stpmon.k.dto.MonsterTypeDto;
 import io.reactivex.rxjava3.core.Observable;
+import okhttp3.ResponseBody;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Streaming;
 
+import javax.json.Json;
 import java.util.List;
 
 public interface PresetApiService {
     @GET("presets/tilesets/{filename}")
-    //TODO: change Type when fixed on server
-    Observable<String> getFile(@Path("filename") String filename);
+    @Streaming //streaming is needed for receiving (larger) files
+    Observable<ResponseBody> getFile(@Path("filename") String filename);
 
     @GET("presets/characters")
     Observable<List<String>> getCharacters();
 
     @GET("presets/characters/{filename}")
-    //TODO: change Type when fixed on server
-    Observable<String> getCharacterFile(@Path("filename") String filename);
+    @Streaming
+    Observable<ResponseBody> getCharacterFile(@Path("filename") String filename);
 
     @GET("presets/monsters")
     Observable<List<MonsterTypeDto>> getMonsters();
@@ -27,7 +30,8 @@ public interface PresetApiService {
     Observable<MonsterTypeDto> getMonster(@Path("id") String id);
 
     @GET("presets/monsters/{id}/image")
-    //TODO: change Type when fixed on server
+    @Streaming
+    //TODO:
     Observable<String> getMonsterImage(@Path("id") String id);
 
     @GET("presets/abilities")
--- src/test/java/de/uniks/stpmon/k/PresetsTestModule.java
+++ src/test/java/de/uniks/stpmon/k/PresetsTestModule.java
@@ -4,11 +4,16 @@ import dagger.Module;
 import dagger.Provides;
 import de.uniks.stpmon.k.dto.AbilityDto;
 import de.uniks.stpmon.k.dto.MonsterTypeDto;
+import de.uniks.stpmon.k.dto.User;
 import de.uniks.stpmon.k.rest.PresetApiService;
 import io.reactivex.rxjava3.core.Observable;
+import okhttp3.ResponseBody;
 
 import javax.inject.Singleton;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Module
 public class PresetsTestModule {
@@ -17,44 +22,127 @@ public class PresetsTestModule {
     @Singleton
     static PresetApiService presetApiService(){
         return new PresetApiService() {
+            final List<String> characters = new ArrayList<>();
+            final List<MonsterTypeDto> monsters = new ArrayList<>();
+            final List<AbilityDto> abilities = new ArrayList<>();
+            final String[] types = {"fire", "water", "grass", "electro", "physics", "ground",
+            "dragon", "poison", "ice", "normal", "dark", "flying", "fighting", "rock", "steel", "bug"};
+
+            /**
+             * Initializes 3 DummyCharacters when getCharacters()
+             * is called but no characters are in the list
+             */
+            private void initDummyCharacters(){
+                String[] c = {"testCharacter1", "testCharacter2", "testCharacter3"};
+                characters.addAll(Arrays.stream(c).toList());
+            }
+
+            /**
+             * Initializes 3 DummyMonsters when getMonsters()
+             * is called but no monsters are in the list
+             */
+            private  void initDummyMonsters(){
+                int amount = 3;
+
+                for(int i = 0; i < amount; i++){
+                    int id = i;
+                    String name = "TestMonster" + i;
+                    String image = "MonsterImage" + i;
+                    String type = types[i % types.length];
+                    String description = "description" + i;
+                    monsters.add(new MonsterTypeDto(id, name, image, type, description));
+                }
+            }
+
+            /**
+             * Initializes 5 DummyAbilities
+             */
+            private void initDummyAbilities(){
+                int amount = 5;
+                for(int i = 0; i < amount; i++){
+                     int id = i;
+                     String type = types[i % types.length];
+                     String name = type + "Ability" + i;
+                     String description = "description" + i;
+                     int maxUses = 3;
+                     double accuracy = 0.5;
+                     int power = 5;
+                    abilities.add(id, name, description, type, maxUses, accuracy, power);
+                }
+
+            }
             @Override
-            public Observable<String> getFile(String filename) {
-                return null;
+            public Observable<ResponseBody> getFile(String filename) {
+                //TODO: save test file somewhere
+                return Observable.empty();
             }
 
             @Override
             public Observable<List<String>> getCharacters() {
-                return null;
+                if(characters.size() == 0){
+                    initDummyCharacters();
+                }
+                return Observable.just(characters);
             }
 
             @Override
-            public Observable<String> getCharacterFile(String filename) {
+            public Observable<ResponseBody> getCharacterFile(String filename) {
                 return null;
             }
 
             @Override
             public Observable<List<MonsterTypeDto>> getMonsters() {
-                return null;
+                if(monsters.size() == 0){
+                    initDummyCharacters();
+                }
+                return Observable.just(monsters);
             }
 
             @Override
             public Observable<MonsterTypeDto> getMonster(String id) {
-                return null;
+                if(monsters.size() == 0){
+                    initDummyMonsters();
+                }
+                Optional<MonsterTypeDto> returnMonster = monsters.stream()
+                        .filter(m -> id.equals(m.id()))
+                        .findFirst();
+
+                return returnMonster.map(r -> Observable.just(returnMonster.get())).orElseGet(()
+                        -> Observable.error(new Throwable("404 Not found")));
             }
 
             @Override
             public Observable<String> getMonsterImage(String id) {
-                return null;
+                if(monsters.size() == 0){
+                    initDummyMonsters();
+                }
+                Optional<MonsterTypeDto> returnMonster = monsters.stream()
+                        .filter(m -> id.equals(m.id()))
+                        .findFirst();
+
+                return returnMonster.map(r -> Observable.just(returnMonster.get().image())).orElseGet(()
+                        -> Observable.error(new Throwable("404 Not found")));
             }
 
             @Override
             public Observable<List<AbilityDto>> getAbilities() {
-                return null;
+                if(abilities.size() == 0){
+                    initDummyAbilities();
+                }
+                return Observable.just(abilities);
             }
 
             @Override
             public Observable<AbilityDto> getAbility(String id) {
-                return null;
+                if(abilities.size() == 0){
+                    initDummyAbilities();
+                }
+                Optional<AbilityDto> returnAbility = abilities.stream()
+                        .filter(a -> id.equals(a.id()))
+                        .findFirst();
+
+                return returnAbility.map(r -> Observable.just(returnAbility.get())).orElseGet(()
+                        -> Observable.error(new Throwable("404 Not found")));
             }
         };
     }
