4aca815d9d3f874df40964ffbb2830e309a83241
sh0xy <53441989+sh0xy@users.noreply.github.com>
2023-05-23 11:24:46 +0200

    feat: add new DTOs

---
+++ src/main/java/de/uniks/stpmon/k/dto/CreateTrainerDto.java
@@ -0,0 +1,8 @@
+package de.uniks.stpmon.k.dto;
+
+public record CreateTrainerDto(
+    String name,
+    String image
+) {
+    
+}
---
+++ src/main/java/de/uniks/stpmon/k/dto/MonsterTypeDto.java
@@ -0,0 +1,13 @@
+package de.uniks.stpmon.k.dto;
+
+import java.util.List;
+
+public record MonsterTypeDto(
+    Integer id,
+    String name,
+    String image,
+    List<String> type, // array because of multi-types
+    String description
+) {
+    
+}
