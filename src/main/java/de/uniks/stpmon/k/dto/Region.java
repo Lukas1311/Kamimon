fd951009bb2c347bff4b532c4577abc02cf13795
sh0xy <53441989+sh0xy@users.noreply.github.com>
2023-05-23 11:29:40 +0200

    refactor: edit/update old DTOs

--- src/main/java/de/uniks/stpmon/k/dto/Region.java
+++ src/main/java/de/uniks/stpmon/k/dto/Region.java
@@ -1,9 +1,14 @@
 package de.uniks.stpmon.k.dto;
 
+import javax.json.Json;
+
 public record Region(
         String createdAt,
         String updatedAt,
         String _id,
-        String name
+        String name,
+        Spawn spawn,
+        Json map // Tiled map in JSON format
+        // TODO: create own record for tiled map ? with custom values that we need
         ) {
 }
--- src/main/java/de/uniks/stpmon/k/dto/User.java
+++ src/main/java/de/uniks/stpmon/k/dto/User.java
@@ -3,6 +3,8 @@ package de.uniks.stpmon.k.dto;
 import java.util.ArrayList;
 
 public record User(
+        String createdAt, // date-time
+        String updatedAt, // date-time
         String _id,
         String name,
         String status,
