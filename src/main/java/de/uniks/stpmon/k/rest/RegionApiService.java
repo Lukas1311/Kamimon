6ea9aca9a22bd3401e8fc86ca4bd6df1f9c4b1eb
basbaer <basbaer@users.noreply.github.com>
2023-05-24 13:21:54 +0200

    feat: add Region Trainer

--- src/main/java/de/uniks/stpmon/k/rest/RegionApiService.java
+++ src/main/java/de/uniks/stpmon/k/rest/RegionApiService.java
@@ -1,24 +1,47 @@
 package de.uniks.stpmon.k.rest;
 
 import de.uniks.stpmon.k.dto.Area;
-import io.reactivex.rxjava3.core.Observable;
+import de.uniks.stpmon.k.dto.CreateTrainerDto;
 import de.uniks.stpmon.k.dto.Region;
-import retrofit2.http.GET;
-import retrofit2.http.Path;
+import de.uniks.stpmon.k.dto.Trainer;
+import io.reactivex.rxjava3.core.Observable;
+import retrofit2.http.*;
 
 import java.util.List;
 
 public interface RegionApiService {
+
+    //---------------- Region Trainers ----------------------------
+    @POST("regions/{regionId}/trainers")
+    Observable<Trainer> createTrainer(@Path("regionId") String regionId,
+                                      @Body CreateTrainerDto trainerDto);
+
+    @GET("regions/{regionId}/trainers")
+    Observable<List<Trainer>> getTrainers(@Path("regionId"),
+                                          @Query("area") String areaId,
+                                          @Query("user") String userId);
+
+    @GET("regions/{regionId}/trainers/{id}")
+        //TODO: regionId?
+    Observable<Trainer> getTrainer(@Path("id") String trainerId
+    );
+
+    @DELETE("regions/{regionId}/trainers/{id}")
+    Observable<Trainer> deleteTrainer(@Path("id") String trainerId);
+
+    //------------------- Regions ---------------------------------
+
     @GET("regions")
     Observable<List<Region>> getRegions();
 
     @GET("regions/{id}")
     Observable<Region> getRegion(@Path("id") String id);
 
+    //---------------- Region Areas ------------------------------
+
     @GET("regions/{region}/areas")
     Observable<List<Area>> getRegions(@Path("region") String region);
 
     @GET("regions/{region}/areas/{id}")
     Observable<Area> getRegion(@Path("region") String region, @Path("id") String id);
-
 }
