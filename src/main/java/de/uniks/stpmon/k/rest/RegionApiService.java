6c21bc923b12199cccd31307e4950ac6fbb77158
discobreak21 <bastianbaer@hotmail.com>
2023-05-24 12:11:42 +0200

    feat: init RegionApiService

--- src/main/java/de/uniks/stpmon/k/rest/RegionApiService.java
+++ src/main/java/de/uniks/stpmon/k/rest/RegionApiService.java
@@ -1,5 +1,6 @@
 package de.uniks.stpmon.k.rest;
 
+import de.uniks.stpmon.k.dto.Area;
 import io.reactivex.rxjava3.core.Observable;
 import de.uniks.stpmon.k.dto.Region;
 import retrofit2.http.GET;
@@ -13,4 +14,11 @@ public interface RegionApiService {
 
     @GET("regions/{id}")
     Observable<Region> getRegion(@Path("id") String id);
+
+    @GET("regions/{region}/areas")
+    Observable<List<Area>> getRegions(@Path("region") String region);
+
+    @GET("regions/{region}/areas/{id}")
+    Observable<Area> getRegion(@Path("region") String region, @Path("id") String id);
+
 }
