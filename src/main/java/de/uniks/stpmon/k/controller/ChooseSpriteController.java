2109a70f32d2215828348336528bbd4f9d6c80b4
halber <XhalberX@gmail.com>
2023-05-27 16:28:51 +0200

    add: ChooseSprite Controller and language translation

--- src/main/java/de/uniks/stpmon/k/controller/ChooseSpriteController.java
+++ src/main/java/de/uniks/stpmon/k/controller/ChooseSpriteController.java
@@ -1,4 +1,39 @@
 package de.uniks.stpmon.k.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+import javax.inject.Inject;
+
 public class ChooseSpriteController extends Controller {
+    @FXML
+    public Text chooseTrainer;
+    @FXML
+    public Button SpriteLeft;
+    @FXML
+    public ImageView SpriteImage;
+    @FXML
+    public Button SpriteRight;
+    @FXML
+    public Button saveSprite;
+
+    @Inject
+    public ChooseSpriteController() {
+    }
+
+    @Override
+    public Parent render() {
+        final Parent parent = super.render();
+
+        chooseTrainer.setText(translateString("chooseTrainer"));
+
+        return parent;
+    }
+
+    public void saveSprite(ActionEvent actionEvent) {
+    }
 }
