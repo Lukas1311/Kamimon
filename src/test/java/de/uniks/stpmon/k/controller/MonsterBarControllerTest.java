7186cf9403df4efefd547f5ec61e0e0902a61944
Nguyen Nguyen <thithanhnguyen.nguyen1308@gmail.com>
2023-05-28 08:26:54 +0200

    feat: add testShowMonsters()

--- src/test/java/de/uniks/stpmon/k/controller/MonsterBarControllerTest.java
+++ src/test/java/de/uniks/stpmon/k/controller/MonsterBarControllerTest.java
@@ -1,8 +1,10 @@
 package de.uniks.stpmon.k.controller;
 
 import de.uniks.stpmon.k.App;
+import javafx.application.Platform;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,13 +16,16 @@ import org.testfx.framework.junit5.ApplicationTest;
 
 import java.util.Objects;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class MonsterBarControllerTest extends ApplicationTest {
     @Mock
     private ImageView monsterSlot;
+
+    @Mock
+    MonsterListController monsterListController;
 
     @Spy
     App app = new App(null);
@@ -67,17 +72,35 @@ public class MonsterBarControllerTest extends ApplicationTest {
     @Test
     public void testSetMonsterStatus_LowHP() throws InterruptedException {
         // Load the image for low HP
-        Image weakHP = new Image(Objects.requireNonNull(getClass().getResource("/de/uniks/stpmon/k/controller/healthPointsLow.png")).toString());
+        Image lowHP = new Image(Objects.requireNonNull(getClass().getResource("/de/uniks/stpmon/k/controller/healthPointsLow.png")).toString());
         // Set the mock
-        when(monsterSlot.getImage()).thenReturn(weakHP);
+        when(monsterSlot.getImage()).thenReturn(lowHP);
 
         monsterBarController.setMonsterStatus(2, 10, 100);
         Thread.sleep(1000);
 
         // Check if the image is the same when HP is lower than 20%
-        assertEquals(weakHP, monsterSlot.getImage());
+        assertEquals(lowHP, monsterSlot.getImage());
     }
 
+    @Test
+    public void testShowMonsters() {
+        Pane monsterList = new Pane();
+        when(monsterListController.render()).thenReturn(monsterList);
 
+        Platform.runLater(() -> {
+            // Show the popup
+            monsterBarController.showMonsters();
 
+            // Check if the monster list is showing
+            assertTrue(monsterBarController.monsterListPopup.isShowing());
+
+            // Hide the popup
+            monsterBarController.showMonsters();
+
+            // Check if the monster list is hidden
+            assertFalse(monsterBarController.monsterListPopup.isShowing());
+
+        });
+    }
 }
