34c339bc7e6cd2e1e3aad0c5d97f89589cb1a46b
sh0xy <53441989+sh0xy@users.noreply.github.com>
2023-05-30 19:35:18 +0200

    feat: add more scenarios + add underscore

--- src/main/java/de/uniks/stpmon/k/controller/popup/PopUpScenario.java
+++ src/main/java/de/uniks/stpmon/k/controller/popup/PopUpScenario.java
@@ -1,8 +1,11 @@
 package de.uniks.stpmon.k.controller.popup;
 
 public enum PopUpScenario {
-    SAVECHANGES("doYouWantToSaveChanges"),
-    DELETEUSER("here text for delete");
+    SAVE_CHANGES("doYouWantToSaveChanges"),
+    DELETE_USER("doYouWantToDeleteUser"),
+    DELETION_CONFIRMATION("userGotDeleted"),
+    UNSAVED_CHANGES("thereAreUnsavedChanges");
+
 
     private final String mainText;
 
