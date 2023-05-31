3b66a9b13f727cdecd9d22e581ada2f20052a432
discobreak21 <bastianbaer@hotmail.com>
2023-05-31 13:11:42 +0200

    feat: add methods from api to RegionService

--- src/main/java/de/uniks/stpmon/k/service/RegionService.java
+++ src/main/java/de/uniks/stpmon/k/service/RegionService.java
@@ -1,9 +1,13 @@
 package de.uniks.stpmon.k.service;
 
+import de.uniks.stpmon.k.dto.CreateTrainerDto;
 import de.uniks.stpmon.k.models.Area;
+import de.uniks.stpmon.k.models.Monster;
 import de.uniks.stpmon.k.models.Region;
+import de.uniks.stpmon.k.models.Trainer;
 import de.uniks.stpmon.k.rest.RegionApiService;
 import de.uniks.stpmon.k.service.storage.RegionStorage;
+import de.uniks.stpmon.k.service.storage.UserStorage;
 import io.reactivex.rxjava3.core.Observable;
 
 import javax.inject.Inject;
@@ -18,10 +22,32 @@ public class RegionService {
     protected RegionStorage regionStorage;
 
     @Inject
+    private UserStorage userStorage;
+
+    @Inject
     public RegionService(RegionApiService regionApiService) {
         this.regionApiService = regionApiService;
     }
 
+    //---------------- Region Trainers ----------------------------
+    public Observable<Trainer> createTrainer(String regionId, String name, String image) {
+        CreateTrainerDto dto = new CreateTrainerDto(name, image);
+        return regionApiService.createTrainer(regionId, dto);
+    }
+
+    public Observable<List<Trainer>> getTrainers(String regionId, String areaId) {
+        return regionApiService.getTrainers(regionId, areaId, userStorage.getUser()._id());
+    }
+
+    public Observable<Trainer> getTrainer(String trainerId) {
+        return regionApiService.getTrainer(trainerId);
+    }
+
+    public Observable<Trainer> deleteTrainer(String trainerId) {
+        return regionApiService.deleteTrainer(trainerId);
+    }
+
+    //------------------- Regions ---------------------------------
     public Observable<List<Region>> getRegions() {
         return regionApiService.getRegions();
     }
@@ -44,11 +70,21 @@ public class RegionService {
         });
     }
 
+    //---------------- Region Areas ------------------------------
     public Observable<Area> getArea(String regionId, String areaId) {
         return regionApiService.getArea(regionId, areaId);
     }
 
     public Observable<List<Area>> getAreas(String regionId) {
         return regionApiService.getAreas(regionId);
+    }
+
+    //------------- Trainer Monsters -------------------------------
+    public Observable<List<Monster>> getMonsters(String trainerId) {
+        return regionApiService.getMonsters(trainerId);
+    }
+
+    public Observable<Monster> getMonster(String monsterId) {
+        return regionApiService.getMonster(monsterId);
     }
 }
