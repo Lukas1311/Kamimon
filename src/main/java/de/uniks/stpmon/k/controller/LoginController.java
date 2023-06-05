a05de21caf764c4e6b91af54c848c42d89329772
sh0xy <53441989+sh0xy@users.noreply.github.com>
2023-06-06 01:44:56 +0200

    refactor: replace with svg log

--- src/main/java/de/uniks/stpmon/k/controller/LoginController.java
+++ src/main/java/de/uniks/stpmon/k/controller/LoginController.java
@@ -6,6 +6,7 @@ import de.uniks.stpmon.k.service.AuthenticationService;
 import de.uniks.stpmon.k.service.NetworkAvailability;
 import de.uniks.stpmon.k.service.UserService;
 import de.uniks.stpmon.k.service.storage.TokenStorage;
+import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.BooleanBinding;
 import javafx.beans.property.SimpleStringProperty;
@@ -146,8 +147,11 @@ public class LoginController extends Controller {
 
 
         //Show KAMIMON Logo
-        imageViewKamimonLettering.setImage(loadImage("kamimonLettering.png"));
-        imageViewKamimonLettering.setPreserveRatio(true);
+        Platform.runLater(() -> {
+            setVectorImage(imageViewKamimonLettering, "kamimonLettering.svg");
+            imageViewKamimonLettering.setPreserveRatio(true);
+        });
+
         return parent;
     }
 
