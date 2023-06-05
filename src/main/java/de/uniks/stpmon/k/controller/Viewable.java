f0d928a62c8daf67e47f15a56bf72665b4f7499b
sh0xy <53441989+sh0xy@users.noreply.github.com>
2023-06-05 21:55:13 +0200

    feat: add load vector method for classes

--- src/main/java/de/uniks/stpmon/k/controller/Viewable.java
+++ src/main/java/de/uniks/stpmon/k/controller/Viewable.java
@@ -1,6 +1,7 @@
 package de.uniks.stpmon.k.controller;
 
 import de.uniks.stpmon.k.App;
+
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Scheduler;
@@ -9,9 +10,23 @@ import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.functions.Consumer;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import javafx.application.Platform;
+import javafx.scene.Group;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.shape.SVGPath;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import javax.inject.Inject;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public abstract class Viewable {
@@ -71,5 +86,83 @@ public abstract class Viewable {
      */
     protected Image loadImage(String image) {
         return new Image(Objects.requireNonNull(Viewable.class.getResource(image)).toString());
+    }
+
+    private class SVGData {
+        private List<SVGPath> svgPaths;
+        private Integer width;
+        private Integer height;
+
+        private SVGData(List<SVGPath> svgPaths, Integer width, Integer height) {
+            this.svgPaths = svgPaths;
+            this.width = width;
+            this.height = height;
+        }
+
+        private List<SVGPath> getSVGPaths() {
+            return this.svgPaths;
+        }
+
+        private Integer getWidth() {
+            return this.width;
+        }
+
+        private Integer getHeight() {
+            return this.height;
+        }
+    }
+
+    private SVGData loadVectorImage(String filename) {
+        URL svgUrl = Objects.requireNonNull(Viewable.class.getResource(filename));
+        List<SVGPath> svgPaths = new ArrayList<>();
+        Integer svgWidth = 0, svgHeight = 0;
+        try {
+            DocumentBuilderFactory  factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            // parse the svg
+            Document document = builder.parse(svgUrl.openStream());
+
+            // get the xml root node (svg is basically made of xml)
+            Element root = document.getDocumentElement();
+            // extract the width and height from the 'viewBox' node
+            String[] viewBoxValues = root.getAttribute("viewBox").split(" ");
+            svgWidth = Integer.parseInt(viewBoxValues[2]);
+            svgHeight = Integer.parseInt(viewBoxValues[3]);
+
+            NodeList elements = root.getElementsByTagName("*");
+            
+            // process all the elements inside the svg
+            for (int i = 0; i < elements.getLength(); i++) {
+                Node node = elements.item(i);
+                Element pathElement = (Element) node;
+
+                // extract relevant information from the SVG element (we only need the paths from the 'd'-node)
+                String elementName = node.getNodeName();
+                if (elementName.equals("path")) {
+                    // 'd' contains one/the svg path
+                    String pathData = pathElement.getAttribute("d");
+
+                    SVGPath path = new SVGPath();
+                    path.setContent(pathData);
+                    svgPaths.add(path);
+                }
+            }
+        } catch (NullPointerException e) {
+            System.err.println("Error: svg vector is null/empty.");
+        } catch (Exception e) {
+            System.err.println("Error: while loading the svg an error occured.");
+        }
+        return new SVGData(svgPaths, svgWidth, svgHeight);
+    }
+
+    protected void setVectorImage(ImageView imageView, String filename) {
+        SVGData svgData = loadVectorImage(filename);
+        List<SVGPath> svgPaths = svgData.getSVGPaths();
+
+        Group vectorGroup = new Group(svgPaths.toArray(new SVGPath[0]));
+        WritableImage image = new WritableImage(svgData.getWidth(), svgData.getHeight());
+        // render the group onto our writeableImage
+        vectorGroup.snapshot(null, image);
+        imageView.setImage(image);
     }
 }
