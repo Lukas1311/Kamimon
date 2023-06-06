bb4a277b34dcfef0e1c08fb5c3023591612094c7
sh0xy <53441989+sh0xy@users.noreply.github.com>
2023-06-06 18:17:14 +0200

    feat: add fxml nodes

--- src/main/java/de/uniks/stpmon/k/controller/MapOverviewController.java
+++ src/main/java/de/uniks/stpmon/k/controller/MapOverviewController.java
@@ -1,11 +1,29 @@
 package de.uniks.stpmon.k.controller;
 
+import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
 
 import javax.inject.Inject;
 
 
 public class MapOverviewController extends Controller {
+
+    @FXML
+    BorderPane mapOverviewContent;
+    @FXML
+    Label regionNameLabel;
+    @FXML
+    Button closeButton;
+    @FXML
+    ImageView mapOverviewImage;
+    @FXML
+    Text buildingDescription;
+
     
     @Inject
     public MapOverviewController() {
