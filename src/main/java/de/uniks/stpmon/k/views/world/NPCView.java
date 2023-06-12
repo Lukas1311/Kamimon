7d3ce26f2c2b64f736e347382bf6807083d6e787
halber <XhalberX@gmail.com>
2023-06-12 21:30:18 +0200

    changed methods to a CollectiveView

--- src/main/java/de/uniks/stpmon/k/views/world/NPCView.java
+++ src/main/java/de/uniks/stpmon/k/views/world/NPCView.java
@@ -1,9 +1,6 @@
 package de.uniks.stpmon.k.views.world;
 
-import de.uniks.stpmon.k.service.RegionService;
-import de.uniks.stpmon.k.service.storage.RegionStorage;
 import de.uniks.stpmon.k.service.storage.TrainerProvider;
-import javafx.scene.Node;
 
 import javax.inject.Inject;
 
@@ -11,21 +8,9 @@ public class NPCView extends EntityView {
 
     @Inject
     TrainerProvider trainerProvider;
-    @Inject
-    RegionService regionService;
-    @Inject
-    RegionStorage regionStorage;
 
     @Inject
     public NPCView() {
-    }
-
-    public Node render() {
-        subscribe(regionService.getNPCs(regionStorage.getRegion().toString()), (list) -> {
-            list.forEach(npc -> getProvider().setTrainer(npc));
-        }, (error) -> {
-        });
-
     }
 
     @Override
