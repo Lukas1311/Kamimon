ed27f62f74f19b37482abd4b68988f587aae96be
sh0xy <53441989+sh0xy@users.noreply.github.com>
2023-06-13 02:19:50 +0200

    feat: add reset to toggle

--- src/main/java/de/uniks/stpmon/k/utils/UiToggle.java
+++ src/main/java/de/uniks/stpmon/k/utils/UiToggle.java
@@ -2,13 +2,20 @@ package de.uniks.stpmon.k.utils;
 
 public class UiToggle {
     private boolean value;
+    private boolean initialValue;
+
 
     public UiToggle(boolean initialValue) {
-        this.value = initialValue;
+        this.initialValue = initialValue;
+        reset();
     }
 
     public boolean toggle() {
         value = !value;
         return value;
+    }
+
+    public void reset() {
+        value = initialValue;
     }
 }
