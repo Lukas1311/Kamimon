88329b04f0c62082fd4c029d10484014bd6ac215
halber <XhalberX@gmail.com>
2023-06-14 20:18:21 +0200

    feat: Highlight Map and by clicking on the highlight the discription is displayed

--- src/main/java/de/uniks/stpmon/k/controller/MapOverviewController.java
+++ src/main/java/de/uniks/stpmon/k/controller/MapOverviewController.java
@@ -1,5 +1,11 @@
 package de.uniks.stpmon.k.controller;
 
+import de.uniks.stpmon.k.models.Region;
+import de.uniks.stpmon.k.service.RegionService;
+import de.uniks.stpmon.k.service.storage.RegionStorage;
+import de.uniks.stpmon.k.service.storage.TrainerStorage;
+import de.uniks.stpmon.k.service.world.TextDeliveryService;
+import de.uniks.stpmon.k.service.world.TextureSetService;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
@@ -8,24 +14,26 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Window;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-
-import de.uniks.stpmon.k.models.Region;
-import de.uniks.stpmon.k.service.storage.RegionStorage;
-import de.uniks.stpmon.k.service.world.TextDeliveryService;
-import de.uniks.stpmon.k.service.world.TextureSetService;
 import java.awt.image.BufferedImage;
 
 
 @Singleton
 public class MapOverviewController extends ToastedController {
 
+    @FXML
+    public Pane highlightPane;
+    @FXML
+    public StackPane mapStackPane;
     @FXML
     BorderPane mapOverviewContent;
     @FXML
@@ -45,11 +53,14 @@ public class MapOverviewController extends ToastedController {
     RegionStorage regionStorage;
     @Inject
     TextDeliveryService textDeliveryService;
+    @Inject
+    TrainerStorage trainerStorage;
+    @Inject
+    RegionService regionService;
 
     private final double MAP_OVERVIEW_SCALE = 0.8; // scale the map container to 80% of screen
     private Image map;
-    private Region currentRegion;
-    
+
     @Inject
     public MapOverviewController() {
     }
@@ -57,71 +68,52 @@ public class MapOverviewController extends ToastedController {
     @Override
     public void init() {
         super.init();
-
-        currentRegion = regionStorage.getRegion();
-
-        if (currentRegion.map() != null) {
-            subscribe(
-                textDeliveryService.getRouteData(currentRegion),
-                routeListData -> {
-                        routeListData.stream()
-                            .filter(routeData -> routeData.id() == 65) // e.g. id = 65 for Coupe Archipelago
-                            .forEach(System.out::println);
-                    // TODO: @halber01 maybe you can also calculate the current position of the player and show him on the map
-                    // TODO: here you could filter each route for id or whatever
-                    // and then calculate the position on the map with the given values inside the data
-                    // e.g. the 3 little isles on the left of Albertania are the "Coupe Archipelago"
-                    // and they have these values height=0, width=0, x=16, y=112
-                }, err -> {
-                    err.printStackTrace();
-                    handleError(err);
-                    // TODO: if you need to set fxelements with text then better put this whole method into render
-                }
-            );
-        }
-
     }
 
 
     @Override
     public Parent render() {
         final Parent parent = super.render();
-        
+        Region currentRegion = regionStorage.getRegion();
         regionNameLabel.setText(currentRegion.name());
-        regionNameLabel.setFont(new Font(20));
-
         if (currentRegion.map() != null) {
             subscribe(
-                textureSetService.createMap(currentRegion),
-                tileMap -> {
-                    BufferedImage renderedMap = tileMap.renderMap();
-                    map = SwingFXUtils.toFXImage(renderedMap, null);
-                    mapImageView.setImage(map);
-                    mapImageView.fitWidthProperty().bind(mapOverviewContent.widthProperty().multiply(MAP_OVERVIEW_SCALE));
-                    mapImageView.fitHeightProperty().bind(mapOverviewContent.heightProperty().multiply(MAP_OVERVIEW_SCALE));
-                    mapContainer.setPrefSize(mapImageView.getFitWidth(), mapImageView.getFitHeight());
-                }, err -> {
-                    handleError(err);
-                }
+                    textureSetService.createMap(currentRegion),
+                    tileMap -> {
+                        BufferedImage renderedMap = tileMap.renderMap();
+                        map = SwingFXUtils.toFXImage(renderedMap, null);
+                        mapImageView.setImage(map);
+                        mapImageView.fitWidthProperty().bind(mapOverviewContent.widthProperty().multiply(0.6));
+                        mapImageView.fitHeightProperty().bind(mapOverviewContent.heightProperty().multiply(MAP_OVERVIEW_SCALE));
+                        mapContainer.setPrefSize(mapImageView.getFitWidth(), mapImageView.getFitHeight());
+                    }, this::handleError
             );
         }
-        
+
         Window parentWindow = app.getStage().getScene().getWindow();
         mapOverviewContent.prefWidthProperty().bind(parentWindow.widthProperty().multiply(MAP_OVERVIEW_SCALE));
         mapOverviewContent.prefHeightProperty().bind(parentWindow.heightProperty().multiply(MAP_OVERVIEW_SCALE));
 
-        mapOverviewContent.setStyle("-fx-background-color: black");
-        System.out.println("test");
+        highlightPane.maxWidthProperty().bind(mapImageView.fitWidthProperty());
+        highlightPane.maxHeightProperty().bind(mapImageView.fitHeightProperty());
+
+        mapStackPane.setScaleX(1.1);
+        mapStackPane.setScaleY(1.1);
 
 
-        closeButton.setOnAction(click -> closeMap());
+        if (currentRegion.map() != null) {
+            subscribe(
+                    textDeliveryService.getRouteData(currentRegion),
+                    routeListData -> routeListData.forEach(routeData -> {
+
+                        Rectangle rectangle = new Rectangle(routeData.x(), routeData.y(), routeData.width(), routeData.height());
+                        rectangle.setFill(Paint.valueOf("#ffffff"));
+                        rectangle.setOpacity(0.3);
+                        highlightPane.getChildren().add(rectangle);
+                        rectangle.setOnMouseClicked(event -> regionDescription.setText(routeData.routeText().description()));
+                    }));
+
+        }
         return parent;
-    }
-
-    public void showInfo() {
-    }
-
-    public void closeMap() {
-        // mapOverviewContent.setVisible(false); // TODO: unused (used already in ingame)
     }
 }
