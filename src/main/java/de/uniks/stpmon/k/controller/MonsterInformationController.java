68d628bad4a0cc57b4958e3de1b3cbd3cc6ee906
Nguyen Nguyen <thithanhnguyen.nguyen1308@gmail.com>
2023-06-14 07:32:31 +0200

    feat: add MonsterInformationController

---
+++ src/main/java/de/uniks/stpmon/k/controller/MonsterInformationController.java
@@ -0,0 +1,89 @@
+package de.uniks.stpmon.k.controller;
+
+import de.uniks.stpmon.k.models.Monster;
+import de.uniks.stpmon.k.service.PresetService;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+
+import javax.inject.Inject;
+import java.util.List;
+
+public class MonsterInformationController extends ToastedController {
+    @FXML
+    public ImageView monsterImage;
+    @FXML
+    public Label speedLabel;
+    @FXML
+    public Label currentHPLabel;
+    @FXML
+    public Label maxHPLabel;
+    @FXML
+    public Label levelLabel;
+    @FXML
+    public Label experienceLabel;
+    @FXML
+    public Text monsterNameText;
+    @FXML
+    public Text descriptionText;
+    @FXML
+    public TextFlow descriptionTextFlow;
+    @FXML
+    public HBox typeListHBox;
+    @FXML
+    public HBox abilitiesHBox;
+
+    @Inject
+    PresetService presetService;
+
+    @Inject
+    public MonsterInformationController() {
+    }
+
+    @Override
+    public Parent render() {
+        return super.render();
+    }
+
+    public void loadMonsterTypeDto(String id) {
+        disposables.add(presetService.getMonster(id)
+                .observeOn(FX_SCHEDULER)
+                .subscribe(monsterTypeDto -> {
+                    monsterNameText.setText(monsterTypeDto.name());
+
+                    List<String> types = monsterTypeDto.type();
+                    updateTypeList(types);
+
+                    descriptionText.setText(monsterTypeDto.description());
+                    descriptionTextFlow.getChildren().clear();
+                    descriptionTextFlow.getChildren().add(descriptionText);
+                }));
+    }
+
+
+
+    private void updateTypeList(List<String> types) {
+        typeListHBox.getChildren().clear();
+        for (String type : types) {
+            Label typeLabel = new Label(type);
+            typeLabel.getStyleClass().add("monster");
+            typeLabel.getStyleClass().add("monster-type");
+            typeListHBox.getChildren().add(typeLabel);
+        }
+    }
+
+    public void loadMonster(Monster monster) {
+        speedLabel.setText(String.valueOf(monster.currentAttributes().speed()));
+
+        maxHPLabel.setText(String.valueOf(monster.attributes().health()));
+        currentHPLabel.setText(String.valueOf(monster.currentAttributes().health()));
+
+        levelLabel.setText(String.valueOf(monster.level()));
+
+        experienceLabel.setText(String.valueOf(monster.experience()));
+    }
+}
