dadc27c8b87f3822b6ba080ec802c8069a098440
Nguyen Nguyen <thithanhnguyen.nguyen1308@gmail.com>
2023-06-14 21:13:47 +0200

    feat: add abilities of the monster

--- src/main/java/de/uniks/stpmon/k/controller/MonsterInformationController.java
+++ src/main/java/de/uniks/stpmon/k/controller/MonsterInformationController.java
@@ -1,15 +1,16 @@
 package de.uniks.stpmon.k.controller;
 
 import de.uniks.stpmon.k.models.Monster;
+import de.uniks.stpmon.k.service.IResourceService;
 import de.uniks.stpmon.k.service.PresetService;
 import de.uniks.stpmon.k.utils.ImageUtils;
-import de.uniks.stpmon.k.utils.ResponseUtils;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
@@ -38,10 +39,12 @@ public class MonsterInformationController extends ToastedController {
     @FXML
     public HBox typeListHBox;
     @FXML
-    public HBox abilitiesHBox;
+    public VBox abilitiesVBox;
 
     @Inject
     PresetService presetService;
+    @Inject
+    IResourceService resourceService;
 
     @Inject
     public MonsterInformationController() {
@@ -60,6 +63,7 @@ public class MonsterInformationController extends ToastedController {
                 .subscribe(monsterTypeDto -> {
                     monsterNameText.setText(monsterTypeDto.name());
 
+                    // Retrieves the list of types and updates the type list UI
                     List<String> types = monsterTypeDto.type();
                     updateTypeList(types);
 
@@ -68,33 +72,78 @@ public class MonsterInformationController extends ToastedController {
                     descriptionTextFlow.getChildren().add(descriptionText);
                 }));
 
-        disposables.add(ResponseUtils.readImage(presetService.getMonsterImage(id))
+        disposables.add(resourceService.getMonsterImage(id)
                 .observeOn(FX_SCHEDULER)
                 .subscribe(imageUrl -> {
+                    // Scale and set the image
                     Image image = ImageUtils.scaledImageFX(imageUrl, SCALE);
-
                     monsterImage.setImage(image);
                 }));
     }
 
     private void updateTypeList(List<String> types) {
+        // Update the UI with a list of types
         typeListHBox.getChildren().clear();
         for (String type : types) {
             Label typeLabel = new Label(type);
-            typeLabel.getStyleClass().add("monster");
+            // Add CSS
             typeLabel.getStyleClass().add("monster-type");
             typeListHBox.getChildren().add(typeLabel);
         }
     }
 
     public void loadMonster(Monster monster) {
+        // Set all labels
         speedLabel.setText(String.valueOf(monster.currentAttributes().speed()));
-
         maxHPLabel.setText(String.valueOf(monster.attributes().health()));
         currentHPLabel.setText(String.valueOf(monster.currentAttributes().health()));
-
         levelLabel.setText(String.valueOf(monster.level()));
-
         experienceLabel.setText(String.valueOf(monster.experience()));
+
+        abilitiesVBox.getChildren().clear();
+
+        // Iterate over the abilities of the monster
+        for (String key : monster.abilities().keySet()) {
+            if (monster.abilities().containsKey(key)) {
+                HBox abilityBox = createAbilityBox(key);
+                abilitiesVBox.getChildren().add(abilityBox);
+            }
+        }
+    }
+
+    private HBox createAbilityBox(String id) {
+        // Create the main container
+        HBox abilityBox = new HBox();
+
+        // Create the container for the ability name and type
+        HBox nameAndTypeBox = new HBox();
+        Label abilityTypeLabel = new Label();
+        Text abilityNameText = new Text();
+
+        // Create the container for displaying the current and maximum uses of the ability
+        HBox amountUsesBox = new HBox();
+        Label currentUsesLabel = new Label();
+        Label slash = new Label(" / ");
+        Label maxUsesLabel = new Label();
+
+        abilityBox.getChildren().addAll(nameAndTypeBox, amountUsesBox);
+        nameAndTypeBox.getChildren().addAll(abilityTypeLabel, abilityNameText);
+        amountUsesBox.getChildren().addAll(currentUsesLabel, slash , maxUsesLabel);
+
+        // Apply CSS styles
+        nameAndTypeBox.getStyleClass().add("ability-name-type-box");
+        abilityTypeLabel.getStyleClass().add("ability-type");
+        amountUsesBox.getStyleClass().add("ability-uses");
+
+        // Subscribe to the ability information from the preset service
+        disposables.add(presetService.getAbility(id)
+                .observeOn(FX_SCHEDULER)
+                .subscribe(ability -> {
+                        abilityTypeLabel.setText(ability.type());
+                        abilityNameText.setText(ability.name());
+                        maxUsesLabel.setText(String.valueOf(ability.maxUses()));
+                }));
+
+        return abilityBox;
     }
 }
