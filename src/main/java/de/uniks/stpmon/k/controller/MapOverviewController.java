2c8168e50886efeb2e2ee0372e7b0f5fbeb45241
Alexander Haar <uk077258@student.uni-kassel.de>
2023-06-15 20:16:55 +0200

    feat: highlight route and adding polygon, because some routes had a polygon withit and not a rectangle

--- src/main/java/de/uniks/stpmon/k/controller/MapOverviewController.java
+++ src/main/java/de/uniks/stpmon/k/controller/MapOverviewController.java
@@ -1,11 +1,14 @@
 package de.uniks.stpmon.k.controller;
 
 import de.uniks.stpmon.k.models.Region;
+import de.uniks.stpmon.k.models.map.layerdata.PolygonPoint;
 import de.uniks.stpmon.k.service.RegionService;
 import de.uniks.stpmon.k.service.storage.RegionStorage;
 import de.uniks.stpmon.k.service.storage.TrainerStorage;
 import de.uniks.stpmon.k.service.world.TextDeliveryService;
 import de.uniks.stpmon.k.service.world.TextureSetService;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.NumberBinding;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
@@ -18,6 +21,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Paint;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Window;
@@ -58,7 +62,8 @@ public class MapOverviewController extends ToastedController {
     @Inject
     RegionService regionService;
 
-    private final double MAP_OVERVIEW_SCALE = 0.8; // scale the map container to 80% of screen
+    private final static double MAP_OVERVIEW_SCALE = 0.8; // scale the map container to 80% of screen
+    private final static int TILE_SIZE = 16;
     private Image map;
 
     @Inject
@@ -77,38 +82,49 @@ public class MapOverviewController extends ToastedController {
         Region currentRegion = regionStorage.getRegion();
         regionNameLabel.setText(currentRegion.name());
         if (currentRegion.map() != null) {
+            int width = currentRegion.map().width() * TILE_SIZE;
+            int height = currentRegion.map().height() * TILE_SIZE;
             subscribe(
                     textureSetService.createMap(currentRegion),
                     tileMap -> {
                         BufferedImage renderedMap = tileMap.renderMap();
                         map = SwingFXUtils.toFXImage(renderedMap, null);
                         mapImageView.setImage(map);
-                        mapImageView.fitWidthProperty().bind(mapOverviewContent.widthProperty().multiply(0.6));
-                        mapImageView.fitHeightProperty().bind(mapOverviewContent.heightProperty().multiply(MAP_OVERVIEW_SCALE));
-                        mapContainer.setPrefSize(mapImageView.getFitWidth(), mapImageView.getFitHeight());
+                        mapImageView.setFitHeight(height);
+                        mapImageView.setFitWidth(width);
                     }, this::handleError
             );
+            NumberBinding binding = Bindings.min(mapOverviewContent.widthProperty().multiply(0.6),
+                    mapOverviewContent.heightProperty().multiply(MAP_OVERVIEW_SCALE));
+            mapStackPane.scaleXProperty().bind(binding.map(v -> v.doubleValue() / width));
+            mapStackPane.scaleYProperty().bind(binding.map(v -> v.doubleValue() / height));
         }
 
         Window parentWindow = app.getStage().getScene().getWindow();
         mapOverviewContent.prefWidthProperty().bind(parentWindow.widthProperty().multiply(MAP_OVERVIEW_SCALE));
         mapOverviewContent.prefHeightProperty().bind(parentWindow.heightProperty().multiply(MAP_OVERVIEW_SCALE));
 
-        highlightPane.maxWidthProperty().bind(mapImageView.fitWidthProperty());
-        highlightPane.maxHeightProperty().bind(mapImageView.fitHeightProperty());
-
-        mapStackPane.setScaleX(1.1);
-        mapStackPane.setScaleY(1.1);
-
-
         if (currentRegion.map() != null) {
             subscribe(
                     textDeliveryService.getRouteData(currentRegion),
                     routeListData -> routeListData.forEach(routeData -> {
-
+                        if (!routeData.polygon().isEmpty()) {
+                            Polygon polygon = new Polygon();
+                            for (PolygonPoint point : routeData.polygon()) {
+                                polygon.getPoints().addAll(Double.valueOf(routeData.x() + point.x()), Double.valueOf(routeData.y() + point.y()));
+                            }
+                            polygon.setFill(Paint.valueOf("#ffffff"));
+                            polygon.setOpacity(0.1);
+                            polygon.setOnMouseClicked(event -> regionDescription.setText(routeData.routeText().description()));
+                            highlightPane.getChildren().add(polygon);
+                            return;
+                        }
+                        if (routeData.width() == 0 || routeData.height() == 0) {
+                            return;
+                        }
                         Rectangle rectangle = new Rectangle(routeData.x(), routeData.y(), routeData.width(), routeData.height());
                         rectangle.setFill(Paint.valueOf("#ffffff"));
-                        rectangle.setOpacity(0.3);
+                        rectangle.setOpacity(0.1);
                         highlightPane.getChildren().add(rectangle);
                         rectangle.setOnMouseClicked(event -> regionDescription.setText(routeData.routeText().description()));
                     }));
--- src/main/java/de/uniks/stpmon/k/models/map/layerdata/ObjectData.java
+++ src/main/java/de/uniks/stpmon/k/models/map/layerdata/ObjectData.java
@@ -1,20 +1,21 @@
 package de.uniks.stpmon.k.models.map.layerdata;
 
-import java.util.List;
-
 import de.uniks.stpmon.k.models.map.Property;
 
-public record ObjectData (
-    int id,
-    String name,
-    List<Property> properties,
-    String type,
-    boolean visible,
-    int rotation,
-    int width,
-    int height,
-    int x,
-    int y
+import java.util.List;
+
+public record ObjectData(
+        int id,
+        String name,
+        List<PolygonPoint> polygon,
+        List<Property> properties,
+        String type,
+        boolean visible,
+        int rotation,
+        int width,
+        int height,
+        int x,
+        int y
 ) {
-    
+
 }
