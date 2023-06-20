a297d122c5397bad3c918ec86a9505d8f0994571
Lukas1311 <lukas.lingmann@web.de>
2023-06-20 13:24:35 +0200

    feat: implement EncounterService methods

--- src/main/java/de/uniks/stpmon/k/rest/RegionApiService.java
+++ src/main/java/de/uniks/stpmon/k/rest/RegionApiService.java
@@ -72,34 +72,34 @@ public interface RegionApiService {
 
     //------------- Region Encounters -------------------------------
     @GET("regions/{region}/encounters")
-    Observable<List<Encounter>> getEncounters();
+    Observable<List<Encounter>> getEncounters(@Path("region") String regionId);
 
     @GET("regions/{region}/encounters/{id}")
-    Observable<Encounter> getEncounter(@Path("region") String region,
+    Observable<Encounter> getEncounter(@Path("region") String regionId,
                                        @Path("id") String id);
 
     //------------- Encounter Opponents -------------------------------
     @GET("regions/{region}/trainers/{trainer}/opponents")
-    Observable<List<Opponent>> getTrainerOpponents(@Path("region") String region,
+    Observable<List<Opponent>> getTrainerOpponents(@Path("region") String regionId,
                                                   @Path("trainer") String trainerId);
 
     @GET("regions/{region}/encounters/{encounter}/opponents")
-    Observable<List<Opponent>> getEncounterOpponents(@Path("region") String region,
+    Observable<List<Opponent>> getEncounterOpponents(@Path("region") String regionId,
                                                       @Path("encounter") String encounterId);
 
     @GET("regions/{region}/encounters/{encounter}/opponents/{id}")
-    Observable<Opponent> getEncounterOpponent(@Path("region") String region,
+    Observable<Opponent> getEncounterOpponent(@Path("region") String regionId,
                                                @Path("encounter") String encounterId,
                                                @Path("id") String id);
 
     @PATCH("regions/{region}/encounters/{encounter}/opponents/{id}") //Make a move or switch monsters
-    Observable<Opponent> makeMove(@Path("region") String region,
+    Observable<Opponent> makeMove(@Path("region") String regionId,
                                    @Path("encounter") String encounterId,
                                    @Path("id") String id,
                                    @Body UpdateOpponentDto opponentDto);
 
     @DELETE("regions/{region}/encounters/{encounter}/opponents/{id}") // Flee from a wild encounter
-    Observable<Opponent> fleeEncounter(@Path("region") String region,
+    Observable<Opponent> fleeEncounter(@Path("region") String regionId,
                                         @Path("encounter") String encounterId,
                                         @Path("id") String id);
 }
---
+++ src/main/java/de/uniks/stpmon/k/service/EncounterService.java
@@ -0,0 +1,82 @@
+package de.uniks.stpmon.k.service;
+
+import de.uniks.stpmon.k.dto.AbilityMove;
+import de.uniks.stpmon.k.dto.ChangeMonsterMove;
+import de.uniks.stpmon.k.dto.UpdateOpponentDto;
+import de.uniks.stpmon.k.models.Encounter;
+import de.uniks.stpmon.k.models.Monster;
+import de.uniks.stpmon.k.models.Opponent;
+import de.uniks.stpmon.k.rest.RegionApiService;
+import de.uniks.stpmon.k.service.storage.cache.MonsterCache;
+import io.reactivex.rxjava3.core.Observable;
+
+import javax.inject.Inject;
+import java.util.List;
+
+public class EncounterService {
+
+    public enum Moves {
+
+        CHANGE_MONSTER("change-monster"),
+        ABILITY("ability");
+
+        private final String move;
+
+        Moves(final String move) {
+            this.move = move;
+        }
+
+        @Override
+        public String toString() {
+            return move;
+        }
+    }
+
+    @Inject
+    RegionApiService regionApiService;
+
+    @Inject
+    MonsterCache monsterCache;
+
+    @Inject
+    public EncounterService() {
+    }
+
+    //---------------- Region Encounters ----------------------------
+
+    public Observable<List<Encounter>> getEncounters(String regionId) {
+        return regionApiService.getEncounters(regionId);
+    }
+
+    public Observable<Encounter> getEncounter(String regionId, String encounterId) {
+        return regionApiService.getEncounter(regionId, encounterId);
+    }
+
+    //---------------- Encounters Opponents ----------------------------
+
+    public Observable<List<Opponent>> getTrainerOpponents(String regionId, String trainerId) {
+        return regionApiService.getTrainerOpponents(regionId, trainerId);
+    }
+
+    public Observable<List<Opponent>> getEncounterOpponents(String regionId, String encounterId) {
+        return regionApiService.getEncounterOpponents(regionId, encounterId);
+    }
+
+    public Observable<Opponent> getEncounterOpponent(String regionId, String encounterId, String id) {
+        return regionApiService.getEncounterOpponent(regionId, encounterId, id);
+    }
+
+    public Observable<Opponent> makeAbilityMove(String regionId, String encounterId, String id, Monster attacker, int ability, Monster target) {
+        UpdateOpponentDto dto = new UpdateOpponentDto(attacker._id(), new AbilityMove(Moves.ABILITY.toString(), ability , target._id()),null);
+        return regionApiService.makeMove(regionId, encounterId, id, dto);
+    }
+
+    public Observable<Opponent> makeChangeMonsterMove(String regionId, String encounterId, String id, Monster currentMonster, Monster nextMonster) {
+        UpdateOpponentDto dto = new UpdateOpponentDto(currentMonster._id(), null, new ChangeMonsterMove(Moves.CHANGE_MONSTER.toString(), nextMonster._id()));
+        return regionApiService.makeMove(regionId, encounterId, id, dto);
+    }
+
+    public Observable<Opponent> fleeEncounter(String regionId, String encounterId, String id) {
+        return regionApiService.fleeEncounter(regionId, encounterId, id);
+    }
+}
