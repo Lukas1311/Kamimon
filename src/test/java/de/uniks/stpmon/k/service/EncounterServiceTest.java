3ad08701ef91ccc62b88534e77e875097b3a097c
Lukas1311 <lukas.lingmann@web.de>
2023-06-20 19:31:56 +0200

    feat: add tests for encounters opponents

--- src/test/java/de/uniks/stpmon/k/service/EncounterServiceTest.java
+++ src/test/java/de/uniks/stpmon/k/service/EncounterServiceTest.java
@@ -3,6 +3,7 @@ package de.uniks.stpmon.k.service;
 import de.uniks.stpmon.k.dto.AbilityMove;
 import de.uniks.stpmon.k.dto.ChangeMonsterMove;
 import de.uniks.stpmon.k.models.Encounter;
+import de.uniks.stpmon.k.models.Monster;
 import de.uniks.stpmon.k.models.Opponent;
 import de.uniks.stpmon.k.models.Result;
 import de.uniks.stpmon.k.rest.RegionApiService;
@@ -15,6 +16,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -149,6 +152,7 @@ class EncounterServiceTest {
         assertEquals("trainer1Id", returnOpponents.get(0).trainer());
         assertEquals(false, returnOpponents.get(0).isAttacker());
         assertEquals(false, returnOpponents.get(0).isNPC());
+        assertEquals("monster1Id", returnOpponents.get(0).monster());
         assertEquals(new AbilityMove("ability", 0, "targetId"), returnOpponents.get(0).abilityMove());
         assertNull(returnOpponents.get(0).changeMonsterMove());
         assertEquals(new Result("ability-success", 0, "effective"), returnOpponents.get(0).result());
@@ -180,6 +184,7 @@ class EncounterServiceTest {
         assertEquals("trainer2Id", returnOpponents.get(0).trainer());
         assertEquals(true, returnOpponents.get(0).isAttacker());
         assertEquals(true, returnOpponents.get(0).isNPC());
+        assertEquals("monster2Id", returnOpponents.get(0).monster());
         assertNull(returnOpponents.get(0).abilityMove());
         assertEquals(new ChangeMonsterMove("change-monster", "monster3Id"), returnOpponents.get(0).changeMonsterMove());
         assertEquals(new Result("monster-changed", null, null), returnOpponents.get(0).result());
@@ -191,21 +196,129 @@ class EncounterServiceTest {
 
     @Test
     void getEncounterOpponent() {
+        Opponent opponent = getDummyOpponents().get(0);
 
+        //define mock
+        when(regionApiService.getEncounterOpponent(any(), any(), any()))
+                .thenReturn(Observable.just(opponent));
+
+        //action
+        final Opponent returnOpponent = encounterService
+                .getEncounterOpponent("regionId", "encounterId", "id")
+                .blockingFirst();
+
+        //check value
+        assertEquals("0", returnOpponent._id());
+        assertEquals("encounter1Id", returnOpponent.encounter());
+        assertEquals("trainer1Id", returnOpponent.trainer());
+        assertEquals(false, returnOpponent.isAttacker());
+        assertEquals(false, returnOpponent.isNPC());
+        assertEquals("monster1Id", returnOpponent.monster());
+        assertEquals(new AbilityMove("ability", 0, "targetId"), returnOpponent.abilityMove());
+        assertNull(returnOpponent.changeMonsterMove());
+        assertEquals(new Result("ability-success", 0, "effective"), returnOpponent.result());
+        assertEquals(0, returnOpponent.coins());
+
+        // check mock
+        verify(regionApiService).getEncounterOpponent(any(), any(), any());
     }
 
     @Test
     void makeAbilityMove() {
+        Opponent opponent = getDummyOpponents().get(0);
 
+        //ability for attacker
+        SortedMap<String, Integer> sortedMap = new TreeMap<>();
+        sortedMap.put("Tackle", 10);
+
+        // Create new monsters
+        Monster attacker = new Monster("0", null, null, null, null, sortedMap, null, null);
+        Monster target = new Monster("1", null, null, null, null, null, null, null);
+
+        //define mock
+        when(regionApiService.makeMove(any(), any(), any(), any()))
+                .thenReturn(Observable.just(opponent));
+
+        //action
+        final Opponent returnOpponent = encounterService
+                .makeAbilityMove("regionId", "encounterId", "id", attacker, attacker.abilities().get("Tackle"), target)
+                .blockingFirst();
+
+        //check value
+        assertEquals("0", returnOpponent._id());
+        assertEquals("encounter1Id", returnOpponent.encounter());
+        assertEquals("trainer1Id", returnOpponent.trainer());
+        assertEquals(false, returnOpponent.isAttacker());
+        assertEquals(false, returnOpponent.isNPC());
+        assertEquals("monster1Id", returnOpponent.monster());
+        assertEquals(new AbilityMove("ability", 0, "targetId"), returnOpponent.abilityMove());
+        assertNull(returnOpponent.changeMonsterMove());
+        assertEquals(new Result("ability-success", 0, "effective"), returnOpponent.result());
+        assertEquals(0, returnOpponent.coins());
+
+        //check mock
+        verify(regionApiService).makeMove(any(), any(), any(), any());
     }
 
     @Test
     void makeChangeMonsterMove() {
+        Opponent opponent = getDummyOpponents().get(1);
 
+        // Create new monsters
+        Monster attacker = new Monster("0", null, null, null, null, null, null, null);
+        Monster nextMonster = new Monster("1", null, null, null, null, null, null, null);
+
+        //define mock
+        when(regionApiService.makeMove(any(), any(), any(), any()))
+                .thenReturn(Observable.just(opponent));
+
+        //action
+        final Opponent returnOpponent = encounterService
+                .makeChangeMonsterMove("regionId", "encounterId", "id", attacker, nextMonster)
+                .blockingFirst();
+
+        //check value
+        assertEquals("1", returnOpponent._id());
+        assertEquals("encounter2Id", returnOpponent.encounter());
+        assertEquals("trainer2Id", returnOpponent.trainer());
+        assertEquals(true, returnOpponent.isAttacker());
+        assertEquals(true, returnOpponent.isNPC());
+        assertEquals("monster2Id", returnOpponent.monster());
+        assertNull(returnOpponent.abilityMove());
+        assertEquals(new ChangeMonsterMove("change-monster", "monster3Id"), returnOpponent.changeMonsterMove());
+        assertEquals(new Result("monster-changed", null, null), returnOpponent.result());
+        assertEquals(1, returnOpponent.coins());
+
+        //check mock
+        verify(regionApiService).makeMove(any(), any(), any(), any());
     }
 
     @Test
     void fleeEncounter() {
+        Opponent opponent = getDummyOpponents().get(1);
 
+        //define mock
+        when(regionApiService.fleeEncounter(any(), any(), any()))
+                .thenReturn(Observable.just(opponent));
+
+        //action
+        final Opponent returnOpponent = encounterService
+                .fleeEncounter("regionId", "encounterId", "id")
+                .blockingFirst();
+
+        //check values
+        assertEquals("1", returnOpponent._id());
+        assertEquals("encounter2Id", returnOpponent.encounter());
+        assertEquals("trainer2Id", returnOpponent.trainer());
+        assertEquals(true, returnOpponent.isAttacker());
+        assertEquals(true, returnOpponent.isNPC());
+        assertEquals("monster2Id", returnOpponent.monster());
+        assertNull(returnOpponent.abilityMove());
+        assertEquals(new ChangeMonsterMove("change-monster", "monster3Id"), returnOpponent.changeMonsterMove());
+        assertEquals(new Result("monster-changed", null, null), returnOpponent.result());
+        assertEquals(1, returnOpponent.coins());
+
+        //check mock
+        verify(regionApiService).fleeEncounter(any(), any(), any());
     }
 }
