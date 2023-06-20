8962e477bf29fa0f3ce55ae84f5f4a26e3081c4c
Schneison <larson.schneider11@gmail.com>
2023-06-20 14:29:57 +0200

    feat: Update api and socket versions

--- src/main/java/de/uniks/stpmon/k/Main.java
+++ src/main/java/de/uniks/stpmon/k/Main.java
@@ -4,10 +4,10 @@ import javafx.application.Application;
 
 public class Main {
     public static final String API_DOMAIN = "stpmon.uniks.de";
-    public static final String API_URL = "https://" + API_DOMAIN + "/api/v2";
-    public static final String WS_URL = "wss://stpmon.uniks.de/ws/v2";
+    public static final String API_URL = "https://" + API_DOMAIN + "/api/v3";
+    public static final String WS_URL = "wss://stpmon.uniks.de/ws/v3";
     public static final String UDP_URL = "stpmon.uniks.de";
-    public static final int UDP_PORT = 30011;
+    public static final int UDP_PORT = 30013;
 
     public static void main(String[] args) {
         Application.launch(App.class, args);
