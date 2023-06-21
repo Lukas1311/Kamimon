d9c59567a1271a56fda8246f9a441823979d8c55
Schneison <larson.schneider11@gmail.com>
2023-06-21 15:32:58 +0200

    feat: Add update dialogue to interaction service

--- src/main/java/de/uniks/stpmon/k/models/NPCInfo.java
+++ src/main/java/de/uniks/stpmon/k/models/NPCInfo.java
@@ -1,7 +1,13 @@
 package de.uniks.stpmon.k.models;
 
+import java.util.List;
+
 public record NPCInfo(
-        Boolean walkRandomly
+        Boolean walkRandomly,
+        boolean encounterOnTalk,
+        boolean canHeal,
+        List<String> starters,
+        List<String> encountered
 ) {
 
 }
--- src/main/java/de/uniks/stpmon/k/service/InteractionService.java
+++ src/main/java/de/uniks/stpmon/k/service/InteractionService.java
@@ -1,21 +1,72 @@
 package de.uniks.stpmon.k.service;
 
+import de.uniks.stpmon.k.models.Dialogue;
+import de.uniks.stpmon.k.models.DialogueOption;
+import de.uniks.stpmon.k.models.NPCInfo;
+import de.uniks.stpmon.k.models.Trainer;
+import de.uniks.stpmon.k.service.storage.InteractionStorage;
+import de.uniks.stpmon.k.service.storage.TrainerStorage;
+import de.uniks.stpmon.k.service.storage.cache.CacheManager;
+import de.uniks.stpmon.k.service.storage.cache.ICache;
+import de.uniks.stpmon.k.service.storage.cache.TrainerAreaCache;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 
 @Singleton
-public class InteractionService {
-    private final Map<Integer, List<Integer>>  trainersByPosition = Collections.synchronizedMap(new HashMap<>());
+public class InteractionService implements ILifecycleService {
+    public static final int DISTANCE_CHECKED_FOR_TRAINERS = 2;
     @Inject
-    InteractionService interactionService;
+    InteractionStorage interactionStorage;
+    @Inject
+    TrainerStorage trainerStorage;
+    @Inject
+    CacheManager cacheManager;
+    private TrainerAreaCache areaCache;
 
     @Inject
     public InteractionService() {
     }
 
+    public Dialogue getDialogue(Trainer trainer) {
+        NPCInfo info = trainer.npc();
+        if(info.canHeal()){
+            return Dialogue.create(new DialogueOption("Can heal ?"));
+        }
+        return null;
+    }
 
+    public void tryUpdateDialogue() {
+        Optional<Trainer> optionalTrainer = getSurroundingTrainers();
+        if (optionalTrainer.isEmpty()) {
+            interactionStorage.setDialogue(null);
+            return;
+        }
+        Trainer trainer = optionalTrainer.get();
+        Dialogue dialogue = getDialogue(trainer);
+        if (dialogue == null) {
+            return;
+        }
+        interactionStorage.setDialogue(dialogue);
+    }
+
+    public Optional<Trainer> getSurroundingTrainers() {
+        if (areaCache == null || areaCache.getStatus() == ICache.Status.DESTROYED) {
+            areaCache = cacheManager.trainerAreaCache();
+        }
+        Trainer trainer = trainerStorage.getTrainer();
+        for (int dX = -DISTANCE_CHECKED_FOR_TRAINERS; dX <= DISTANCE_CHECKED_FOR_TRAINERS; dX++) {
+            for (int dY = -DISTANCE_CHECKED_FOR_TRAINERS; dY <= DISTANCE_CHECKED_FOR_TRAINERS; dY++) {
+                int x = trainer.x() + dX;
+                int y = trainer.y() + dY;
+                // Upper bounds are not important because cache is designed for  up to 0xFFFF
+                Optional<Trainer> trainerOptional = areaCache.getTrainerAt(x, y);
+                if (trainerOptional.isPresent()) {
+                    return trainerOptional;
+                }
+            }
+        }
+        return Optional.empty();
+    }
 }
