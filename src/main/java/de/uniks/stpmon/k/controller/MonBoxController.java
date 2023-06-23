e33b65a3202fc715f692db90ea5c05755e9f5beb
Alexander Haar <uk077258@student.uni-kassel.de>
2023-06-23 16:33:15 +0200

    feat: trying to load images from Monster

--- src/main/java/de/uniks/stpmon/k/controller/MonBoxController.java
+++ src/main/java/de/uniks/stpmon/k/controller/MonBoxController.java
@@ -1,12 +1,20 @@
 package de.uniks.stpmon.k.controller;
 
+import de.uniks.stpmon.k.models.Trainer;
+import de.uniks.stpmon.k.service.IResourceService;
+import de.uniks.stpmon.k.service.TrainerService;
+import de.uniks.stpmon.k.service.storage.cache.CacheManager;
+import de.uniks.stpmon.k.service.storage.cache.MonsterCache;
+import de.uniks.stpmon.k.utils.ImageUtils;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 
 import javax.inject.Inject;
+import javax.inject.Provider;
 import javax.inject.Singleton;
 
 @Singleton
@@ -20,12 +28,17 @@ public class MonBoxController extends Controller {
     public GridPane monStorage;
     @FXML
     public ImageView monBoxImage;
+    @Inject
+    Provider<TrainerService> trainerServiceProvider;
+    @Inject
+    Provider<CacheManager> cacheManagerProvider;
+    @Inject
+    IResourceService resourceService;
 
 
     @Inject
     public MonBoxController() {
     }
-
 
     @Override
     public Parent render() {
@@ -33,7 +46,27 @@ public class MonBoxController extends Controller {
 
         loadImage(monBoxImage, "monGrid_v4.png");
 
+        Trainer trainer = trainerServiceProvider.get().getMe();
+        MonsterCache monsterCache = cacheManagerProvider.get().requestMonsters(trainer._id());
+        subscribe(monsterCache.getValues(), monsters -> monsters.forEach(monster -> {
+            ImageView monsterImage = new ImageView();
+            monsterImage.setFitHeight(67);
+            monsterImage.setFitWidth(67);
+
+
+            subscribe(resourceService.getMonsterImage(String.valueOf(monster.type())), image -> {
+                Image monsterImagePNG = ImageUtils.scaledImageFX(image, 4);
+                monsterImage.setImage(monsterImagePNG);
+            });
+            monTeam.add(monsterImage, 0, 0);
+        }));
+
 
         return parent;
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
     }
 }
