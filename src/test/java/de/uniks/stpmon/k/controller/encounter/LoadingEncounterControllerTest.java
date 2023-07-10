8f884efd28f344abebab162278ecb8d4a9d6fbb9
IsmailHalli <uk083810@student.uni-kassel.de>
2023-07-11 00:52:10 +0200

    feat: showScreen test

--- src/test/java/de/uniks/stpmon/k/controller/encounter/LoadingEncounterControllerTest.java
+++ src/test/java/de/uniks/stpmon/k/controller/encounter/LoadingEncounterControllerTest.java
@@ -1,12 +1,20 @@
 package de.uniks.stpmon.k.controller.encounter;
 
 import de.uniks.stpmon.k.App;
+import de.uniks.stpmon.k.models.Trainer;
+import de.uniks.stpmon.k.models.builder.TrainerBuilder;
+import de.uniks.stpmon.k.service.EffectContext;
 import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.testfx.framework.junit5.ApplicationTest;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -17,10 +25,23 @@ public class LoadingEncounterControllerTest extends ApplicationTest {
     @InjectMocks
     LoadingEncounterController loadingEncounterController;
 
+    @Spy
+    final EffectContext effectContext = new EffectContext()
+            .setSkipLoadImages(true);
+
 
     @Override
     public void start(Stage stage) throws Exception {
         app.start(stage);
         loadingEncounterController = new LoadingEncounterController();
+    }
+
+    @Test
+    public void showScreen() {
+        effectContext.setSkipLoading(false);
+        loadingEncounterController.render();
+
+        // Check if the loading encounter screen is shown
+        verify(app).show(any());
     }
 }
