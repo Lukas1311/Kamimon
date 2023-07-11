8652fe84ac3788e885ee1bdbad94132cbd075278
André Falb <andrefalb@gmail.com>
2023-07-11 23:48:00 +0200

    feat: fixed Battlelog

--- src/main/java/de/uniks/stpmon/k/controller/action/ActionFieldBattleLogController.java
+++ src/main/java/de/uniks/stpmon/k/controller/action/ActionFieldBattleLogController.java
@@ -9,12 +9,15 @@ import de.uniks.stpmon.k.models.*;
 import de.uniks.stpmon.k.service.MonsterService;
 import de.uniks.stpmon.k.service.PresetService;
 import de.uniks.stpmon.k.service.SessionService;
-import de.uniks.stpmon.k.service.TrainerService;
 import de.uniks.stpmon.k.service.storage.EncounterStorage;
 import de.uniks.stpmon.k.service.storage.TrainerStorage;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
@@ -25,7 +28,10 @@ import java.util.*;
 public class ActionFieldBattleLogController extends Controller {
 
     @FXML
-    public Text logText;
+    public ScrollPane scrollPane;
+
+    @FXML
+    public VBox vBox;
 
     @Inject
     Provider<ActionFieldController> actionFieldControllerProvider;
@@ -50,16 +56,79 @@ public class ActionFieldBattleLogController extends Controller {
 
     private final List<String> texts = new ArrayList<>();
 
+    private boolean initialized = false;
+
     @Inject
     public ActionFieldBattleLogController() {
+
     }
 
     @Override
     public Parent render() {
         Parent parent = super.render();
 
+        scrollPane.needsLayoutProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue) {
+                scrollPane.setVvalue(1.0);
+            }
+        });
+
+        initListeners();
+
+        Timer myTimer = new Timer();
+
+        onDestroy(myTimer::cancel);
+        myTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+            javafx.application.Platform.runLater(() -> {
+                vBox.getChildren().clear();
+                actionFieldControllerProvider.get().openMainMenu();
+
+            });
+            }}, 5000);
+
+        scrollPane.setOnMouseClicked(event -> {
+            vBox.getChildren().clear();
+            actionFieldControllerProvider.get().openMainMenu();
+        });
+
+        return parent;
+    }
+
+
+    private MonsterTypeDto getTypeForSlot(EncounterSlot slot) {
+        Monster monster = sessionService.getMonster(slot);
+        // Blocking can be used here because values are already loaded in the cache
+        return presetService.getMonster(monster.type()).blockingFirst();
+    }
+
+
+    public void addTextSection(String text, boolean overwriteAll) {
+        //replace whole text in battle log
+        if (overwriteAll) {
+            vBox.getChildren().clear();
+            //texts.clear();
+            //texts.add(text);
+        }
+        //else if (texts.size() >= 2) {
+        //    texts.remove(0);
+        //}
+        Label text1 = new Label(text + "\n");
+        text1.setWrapText(true);
+        text1.setMaxWidth(290);
+        vBox.getChildren().add(text1);
+        //texts.add(text);
+        //renewText();
+    }
+
+    private void initListeners(){
         for (EncounterSlot slot : sessionService.getSlots()) {
-            subscribe(sessionService.listenOpponent(slot), opp -> {
+            subscribe(sessionService.listenOpponent(slot).skip(1), opp -> {
+
+                if (encounterStorage.getSession() == null) {
+                    return;
+                }
                 MonsterTypeDto monster = getTypeForSlot(slot);
 
                 if (opp.move() instanceof AbilityMove move) {
@@ -137,50 +206,17 @@ public class ActionFieldBattleLogController extends Controller {
                 }
             });
         }
-
-
-        Timer myTimer = new Timer();
-
-        onDestroy(myTimer::cancel);
-        myTimer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-            javafx.application.Platform.runLater(() -> actionFieldControllerProvider.get().openMainMenu());
-            }}, 3000);
-
-        logText.setOnMouseClicked(event -> actionFieldControllerProvider.get().openMainMenu());
-
-        return parent;
-    }
-
-    private MonsterTypeDto getTypeForSlot(EncounterSlot slot) {
-        Monster monster = sessionService.getMonster(slot);
-        // Blocking can be used here because values are already loaded in the cache
-        return presetService.getMonster(monster.type()).blockingFirst();
-    }
-
-
-    public void addTextSection(String text, boolean overwriteAll) {
-        //replace whole text in battle log
-        if (overwriteAll) {
-            texts.clear();
-            texts.add(text);
-
-        } else if (texts.size() >= 2) {
-            texts.remove(0);
-        }
-
-        texts.add(text);
-        renewText();
     }
 
 
     private void renewText() {
-        StringBuilder sb = new StringBuilder();
+        //StringBuilder sb = new StringBuilder();
         for (String str : texts) {
-            sb.append(str).append("\n");
+            //sb.append(str).append("\n");
+
+
         }
-        logText.setText(sb.toString());
+        //logText.setText(sb.toString());
     }
 
     @Override
