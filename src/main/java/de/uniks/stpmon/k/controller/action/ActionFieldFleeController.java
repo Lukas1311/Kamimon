f63b365f253f555f671d5a0f01d8acbe600adbab
IsmailHalli <uk083810@student.uni-kassel.de>
2023-07-12 20:23:02 +0200

    fix: feat subscribe

--- src/main/java/de/uniks/stpmon/k/controller/action/ActionFieldFleeController.java
+++ src/main/java/de/uniks/stpmon/k/controller/action/ActionFieldFleeController.java
@@ -7,10 +7,12 @@ import de.uniks.stpmon.k.controller.sidebar.MainWindow;
 import de.uniks.stpmon.k.di.HttpModule_AuthApiFactory;
 import de.uniks.stpmon.k.service.EncounterService;
 import de.uniks.stpmon.k.service.storage.EncounterStorage;
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
@@ -47,11 +49,21 @@ public class ActionFieldFleeController extends Controller {
 
     private void fleeFromWildEncounter() {
         //IngameController.disableEncounter = true;
+        subscribe(encounterService.fleeEncounter(), e -> {
+            HybridController controller = hybridControllerProvider.get();
+            app.show(controller);
+            controller.openMain(MainWindow.INGAME);
+            //pauseTransition.play();
+
+        });
+        /*loadImage(blackScreen, "action/blackScreen.png");
+        PauseTransition pauseTransition = new PauseTransition(Duration.seconds(2));
+        pauseTransition.setOnFinished(e -> {
+        });
+
+         */
 
 
-        HybridController controller = hybridControllerProvider.get();
-        app.show(controller);
-        controller.openMain(MainWindow.INGAME);
     }
 
     @Override
