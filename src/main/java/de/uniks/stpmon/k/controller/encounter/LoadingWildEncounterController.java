6fc1f2c68cce89c3cb7f6291cc5755124705843f
IsmailHalli <uk083810@student.uni-kassel.de>
2023-07-13 23:26:06 +0200

    fix: animation starts before wild encount starts

--- src/main/java/de/uniks/stpmon/k/controller/encounter/LoadingWildEncounterController.java
+++ src/main/java/de/uniks/stpmon/k/controller/encounter/LoadingWildEncounterController.java
@@ -1,19 +1,17 @@
 package de.uniks.stpmon.k.controller.encounter;
 
 import de.uniks.stpmon.k.controller.Controller;
-import javafx.animation.*;
+import javafx.animation.FadeTransition;
+import javafx.animation.ParallelTransition;
+import javafx.animation.PauseTransition;
+import javafx.animation.SequentialTransition;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Arc;
-import javafx.scene.shape.ArcType;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
-import javafx.scene.image.ImageView;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
@@ -23,15 +21,12 @@ import java.util.List;
 
 public class LoadingWildEncounterController extends Controller {
 
-    private static final double START_ANGLE = 90;
-    private static final double ANIMATION_DURATION = 10.0;
-
     @FXML
     Pane fullBox;
 
 
 
-    private List<Rectangle> rectangles = new ArrayList<>();;
+    private final List<Rectangle> rectangles = new ArrayList<>();;
 
     @Inject
     Provider<EncounterOverviewController> encounterOverviewControllerProvider;
@@ -79,11 +74,9 @@ public class LoadingWildEncounterController extends Controller {
 
     private ParallelTransition setUpTransition(){
         ParallelTransition parallelTransition = new ParallelTransition();
-        int i = 0;
         for(Rectangle r : rectangles){
             TranslateTransition transition = new TranslateTransition(Duration.seconds(1), r);
             transition.setToX(0);
-            i++;
             parallelTransition.getChildren().add(transition);
         }
         return parallelTransition;
