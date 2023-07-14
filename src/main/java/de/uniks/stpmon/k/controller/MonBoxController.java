58bf9c3d28806881cfbb6bb1e7895b95c63d9cfc
halber <XhalberX@gmail.com>
2023-07-14 14:41:35 +0200

    feat: drag and drop

--- src/main/java/de/uniks/stpmon/k/controller/MonBoxController.java
+++ src/main/java/de/uniks/stpmon/k/controller/MonBoxController.java
@@ -3,9 +3,11 @@ package de.uniks.stpmon.k.controller;
 import de.uniks.stpmon.k.models.Monster;
 import de.uniks.stpmon.k.models.Trainer;
 import de.uniks.stpmon.k.service.IResourceService;
+import de.uniks.stpmon.k.service.TrainerService;
 import de.uniks.stpmon.k.service.storage.TrainerStorage;
 import de.uniks.stpmon.k.service.storage.cache.CacheManager;
 import de.uniks.stpmon.k.service.storage.cache.MonsterCache;
+import de.uniks.stpmon.k.service.storage.cache.TrainerCache;
 import de.uniks.stpmon.k.utils.ImageUtils;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
@@ -21,6 +23,7 @@ import javafx.scene.layout.VBox;
 import javax.inject.Inject;
 import javax.inject.Provider;
 import javax.inject.Singleton;
+import java.util.ArrayList;
 import java.util.List;
 
 @Singleton
@@ -47,8 +50,16 @@ public class MonBoxController extends Controller {
     IResourceService resourceService;
     @Inject
     Provider<IngameController> ingameControllerProvider;
+    @Inject
+    TrainerService trainerService;
     private MonsterCache monsterCache;
+    private TrainerCache trainerCache;
     private Monster activeMonster;
+    private ImageView monImage;
+    private List<String> monTeamList = new ArrayList<>();
+    private List<String> monStorageList = new ArrayList<>();
+    private int monsterIndexTeam = 0;
+    private int monsterIndexStorage = 0;
 
 
     @Inject
@@ -60,6 +71,9 @@ public class MonBoxController extends Controller {
         final Parent parent = super.render();
         Trainer trainer = trainerStorage.getTrainer();
         monsterCache = cacheManager.requestMonsters(trainer._id());
+        trainerCache = cacheManager.trainerCache();
+        targetGrid(monStorage);
+        targetGrid(monTeam);
         subscribe(monsterCache.getTeam().getValues(), this::showTeamMonster);
         subscribe(monsterCache.getValues(), this::showMonsterList);
         loadImage(monBoxImage, "monGrid_v4.png");
@@ -68,14 +82,16 @@ public class MonBoxController extends Controller {
     }
 
     private void showTeamMonster(List<Monster> monsters) {
-        int monsterIndex = 0;
-        // Team Monster max 6 slots
+        monsterIndexTeam = 0;
+        monTeamList = new ArrayList<>();
 
+        // Team Monster max 6 slots
         for (Monster monster : monsters) {
             ImageView imageView = createMonsterImageView(monster);
-            monTeam.add(imageView, monsterIndex, 0);
+            monTeam.add(imageView, monsterIndexTeam, 0);
             monBoxVbox.toFront();
-            monsterIndex++;
+            monTeamList.add(monster._id());
+            monsterIndexTeam++;
         }
     }
 
@@ -83,17 +99,19 @@ public class MonBoxController extends Controller {
         List<Monster> teamMonsters = monsterCache.getTeam().getValues().blockingFirst();
         int columnCount = 6;
         int rowCount = 5;
-        int monsterIndex = 0;
         monsters.removeAll(teamMonsters);
+        monsterIndexStorage = 0;
+        monStorageList = new ArrayList<>();
 
         for (int row = 0; row < rowCount; row++) {
             for (int column = 0; column < columnCount; column++) {
-                if (monsterIndex < monsters.size()) {
-                    Monster monster = monsters.get(monsterIndex);
+                if (monsterIndexStorage < monsters.size()) {
+                    Monster monster = monsters.get(monsterIndexStorage);
                     ImageView imageView = createMonsterImageView(monster);
                     monStorage.add(imageView, column, row);
                     monBoxVbox.toFront();
-                    monsterIndex++;
+                    monStorageList.add(monster._id());
+                    monsterIndexStorage++;
                 }
             }
         }
@@ -108,45 +126,11 @@ public class MonBoxController extends Controller {
             // Scale and set the image for the Clipboard
             Image image = ImageUtils.scaledImageFX(imageUrl, 0.5);
             imageView.setImage(image);
-            makeDraggable(imageView);
+            draggableimage(imageView);
         });
 
         imageView.setOnMouseClicked(e -> triggerMonsterInformation(monster));
         return imageView;
-    }
-
-    double orgSceneX, orgSceneY;
-
-    private void makeDraggable(ImageView imageView) {
-        imageView.setOnDragDetected(event -> {
-            Dragboard dragboard = imageView.startDragAndDrop(TransferMode.ANY);
-            ClipboardContent content = new ClipboardContent();
-            content.putImage(imageView.getImage());
-            dragboard.setContent(content);
-            orgSceneX = event.getSceneX();
-            orgSceneY = event.getSceneY();
-            event.consume();
-        });
-
-        monTeam.setOnDragOver(event -> {
-            if (event.getGestureSource() != monTeam && event.getDragboard().hasImage()) {
-                event.acceptTransferModes(TransferMode.MOVE);
-            }
-            event.consume();
-        });
-
-        monTeam.setOnDragDropped(event -> {
-            Dragboard dragboard = event.getDragboard();
-            if (dragboard.hasImage()) {
-                System.out.println(event.getSource());
-                monTeam.getChildren().remove(imageView);
-                monTeam.add(imageView, 2, 0);
-
-                event.setDropCompleted(true);
-            }
-            event.consume();
-        });
-
     }
 
     private void openMonsterInformation(Monster monster) {
@@ -171,4 +155,76 @@ public class MonBoxController extends Controller {
         ingameControllerProvider.get().removeChildren(2);
         activeMonster = null;
     }
+
+
+    private ImageView draggableimage(ImageView imageView) {
+        {
+            imageView.setOnDragDetected(event -> {
+                Dragboard dragboard = imageView.startDragAndDrop(TransferMode.MOVE);
+                ClipboardContent content = new ClipboardContent();
+                content.putImage(imageView.getImage());
+                dragboard.setContent(content);
+                monImage = imageView;
+                event.consume();
+            });
+
+            imageView.setOnDragDone(event -> {
+                if (event.getTransferMode() == TransferMode.MOVE) {
+
+                    monImage = null;
+                }
+                event.consume();
+            });
+
+            return imageView;
+        }
+
+    }
+
+    private GridPane targetGrid(GridPane gridPane) {
+        gridPane.setOnDragOver(event -> {
+            if (event.getGestureSource() != gridPane && event.getDragboard().hasImage()) {
+                event.acceptTransferModes(TransferMode.MOVE);
+            }
+            event.consume();
+        });
+
+        gridPane.setOnDragDropped(event -> {
+            Dragboard dragboard = event.getDragboard();
+            if (dragboard.hasImage()) {
+
+
+                ImageView imageView = monImage;
+
+                if (gridPane.equals(monStorage) && !monStorage.getChildren().contains(imageView) && monTeamList.size() > 1) {
+                    int tmp_index = monsterIndexTeam - 1;
+                    String mon = monTeamList.get(tmp_index);
+                    monTeamList.remove(mon);
+                    monTeam.getChildren().remove(imageView);
+
+                    monStorageList.add(mon);
+                    monStorage.add(imageView, monsterIndexStorage, 0);
+                    monsterIndexStorage++;
+                }
+                if (gridPane.equals(monTeam) && !monTeam.getChildren().contains(imageView)) {
+                    if (monTeamList.size() < 6) {
+                        int tmp_index = monsterIndexStorage - 1;
+                        String mon = monStorageList.get(tmp_index);
+                        monStorageList.remove(mon);
+                        monStorage.getChildren().remove(imageView);
+
+                        monTeamList.add(mon);
+                        monTeam.add(imageView, monsterIndexTeam, 0);
+                        monsterIndexTeam++;
+                    }
+                }
+                event.setDropCompleted(true);
+            }
+            event.consume();
+        });
+
+        return gridPane;
+    }
+
+
 }
