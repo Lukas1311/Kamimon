198f54d1eb0b9475d5f9b7362eee5f5c5f2dea34
Schneison <larson.schneider11@gmail.com>
2023-07-17 19:24:13 +0200

    feat: Add constants (like flags) for the tile system

---
+++ src/main/java/de/uniks/stpmon/k/constants/TileConstants.java
@@ -0,0 +1,11 @@
+package de.uniks.stpmon.k.constants;
+
+public class TileConstants {
+
+    public static final int FLAG_ALL = 0xF0000000;
+    public static final int FLAG_FLIPPED_HORIZONTALLY = 0x80000000;
+    public static final int FLAG_FLIPPED_VERTICALLY = 0x40000000;
+    public static final int FLIPPED_DIAGONALLY_FLAG = 0x20000000;
+    public static final int ROTATED_HEXAGONAL_120_FLAG = 0x10000000;
+    public static final int TILE_SIZE = 16;
+}
