0559113cad53848319b88024ea55b31ae49aa6ff
sh0xy <53441989+sh0xy@users.noreply.github.com>
2023-08-01 18:43:32 +0200

    fix: mondex package path

---
+++ src/main/java/de/uniks/stpmon/k/controller/mondex/MonDexController.java
@@ -0,0 +1,111 @@
+package de.uniks.stpmon.k.controller.mondex;
+
+import de.uniks.stpmon.k.controller.Controller;
+import de.uniks.stpmon.k.controller.IngameController;
+import de.uniks.stpmon.k.dto.MonsterTypeDto;
+import de.uniks.stpmon.k.service.PresetService;
+import de.uniks.stpmon.k.service.ResourceService;
+import de.uniks.stpmon.k.service.TrainerService;
+import de.uniks.stpmon.k.utils.ImageUtils;
+import de.uniks.stpmon.k.views.DexCell;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+import javax.inject.Singleton;
+import java.awt.image.BufferedImage;
+
+@Singleton
+public class MonDexController extends Controller {
+
+    @FXML
+    public AnchorPane monDexPain;
+    @FXML
+    public ListView<MonsterTypeDto> dexList;
+
+    @Inject
+    Provider<IngameController> ingameControllerProvider;
+    @Inject
+    PresetService presetService;
+    @Inject
+    Provider<ResourceService> resourceServiceProvider;
+    @Inject
+    Provider<TrainerService> trainerServiceProvider;
+
+    private final ObservableList<MonsterTypeDto> allMonsters = FXCollections.observableArrayList();
+
+    private MonsterTypeDto activeDetail;
+
+    @Inject
+    public MonDexController() {
+
+    }
+
+    @Override
+    public Parent render() {
+        Parent parent = super.render();
+        loadBgImage(monDexPain, getResourcePath() + "monDexBox.png");
+
+        subscribe(presetService.getMonsters(), (monList) -> {
+            if (!monList.isEmpty()) {
+                //allMonsters.addAll(monList.subList(0, 10));
+                allMonsters.addAll(monList);
+                dexList.setCellFactory(param -> new DexCell(this, trainerServiceProvider));
+
+                dexList.setItems(FXCollections.observableArrayList(allMonsters));
+            }
+        });
+
+
+        return parent;
+    }
+
+    public void triggerDetail(MonsterTypeDto mon) {
+        if (activeDetail == null) {
+            openDetail(mon);
+        } else {
+            if (activeDetail == mon) {
+                closeDetail();
+            } else {
+                closeDetail();
+                openDetail(mon);
+            }
+        }
+    }
+
+    private void openDetail(MonsterTypeDto mon) {
+        activeDetail = mon;
+        ingameControllerProvider.get().openMonDexDetail(mon);
+    }
+
+    private void closeDetail() {
+        ingameControllerProvider.get().removeChildren(2);
+        activeDetail = null;
+    }
+
+    public void setMonDexImage(MonsterTypeDto monster, boolean isEncountered, ImageView monImage) {
+        if (effectContext != null && effectContext.shouldSkipLoadImages()) {
+            return;
+        }
+        subscribe(resourceServiceProvider.get().getMonsterImage(String.valueOf(monster.id())), bufferedImage -> {
+            BufferedImage bufImg = bufferedImage;
+            if (!isEncountered) {
+                bufImg = ImageUtils.blackOutImage(bufferedImage);
+            }
+            Image image = ImageUtils.scaledImageFX(bufImg, 1.0);
+            monImage.setImage(image);
+        });
+    }
+
+    @Override
+    public String getResourcePath() {
+        return "mondex/";
+    }
+}
---
+++ src/main/java/de/uniks/stpmon/k/controller/mondex/MonDexEntryController.java
@@ -0,0 +1,52 @@
+package de.uniks.stpmon.k.controller.mondex;
+
+import de.uniks.stpmon.k.controller.Controller;
+import de.uniks.stpmon.k.dto.MonsterTypeDto;
+import de.uniks.stpmon.k.service.TrainerService;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+import javax.inject.Provider;
+
+public class MonDexEntryController extends Controller {
+
+    @FXML
+    public ImageView monImage;
+    @FXML
+    public Label nameLabel;
+    @FXML
+    public Label typeLabel;
+
+    private final MonDexController monDexController;
+    private final MonsterTypeDto monster;
+    private final boolean isEncountered;
+
+    public MonDexEntryController(MonDexController monDexController, MonsterTypeDto entry,
+                                 Provider<TrainerService> trainerServiceProvider) {
+        this.monDexController = monDexController;
+        this.monster = entry;
+        this.isEncountered = trainerServiceProvider.get().getMe().encounteredMonsterTypes().contains(entry.id());
+    }
+
+    @Override
+    public Parent render() {
+        final Parent parent = super.render();
+        typeLabel.setId("type" + monster.id() + "Label");
+
+        monDexController.setMonDexImage(monster, isEncountered, monImage);
+
+        String name = isEncountered ? monster.name() : "???";
+        nameLabel.setText(name);
+
+        typeLabel.setText("#" + monster.id());
+
+        return parent;
+    }
+
+    @Override
+    public String getResourcePath() {
+        return "mondex/";
+    }
+}
