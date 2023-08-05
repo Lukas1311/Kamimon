eaede4d5763c06a773a3d3fa05130b2ffff638c2
discobreak21 <bastianbaer@hotmail.com>
2023-08-05 11:46:58 +0200

    test: remove warnings

--- src/test/java/de/uniks/stpmon/k/AppTest.java
+++ src/test/java/de/uniks/stpmon/k/AppTest.java
@@ -15,8 +15,10 @@ import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -30,8 +32,7 @@ import java.util.ArrayList;
 
 import static java.util.function.Predicate.not;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.testfx.api.FxAssert.verifyThat;
 import static org.testfx.assertions.api.Assertions.assertThat;
 import static org.testfx.util.NodeQueryUtils.hasText;
@@ -519,7 +520,7 @@ class AppTest extends ApplicationTest {
         TestHelper.addMonster(component.trainerStorage(), component);
 
         //test monDex
-        /*clickOn("#backpackImage");
+        clickOn("#backpackImage");
         waitForFxEvents();
         verifyThat("#backpackMenuListView", Node::isVisible);
         waitForFxEvents();
@@ -555,7 +556,7 @@ class AppTest extends ApplicationTest {
         clickOn("#mdmzSettings");
         final Slider musicSlider = lookup("#music").query();
         assertThat(musicSlider.getValue()).isEqualTo(0);
-        clickOn("#settings");*/
+        clickOn("#settings");
 
         //test evolution of mon
         // walk to the right and start Encounter
@@ -573,6 +574,7 @@ class AppTest extends ApplicationTest {
         // attack
         clickOn("#ability_1");
         waitForFxEvents();
+        clickOn("#battleLog");
         clickOn("#battleLog");
         clickOn("#battleLog");
         clickOn("#battleLog");
