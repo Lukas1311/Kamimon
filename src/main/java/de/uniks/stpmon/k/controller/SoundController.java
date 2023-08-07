2d073f839e86ba856bb97f05d17f2af7dee400fe
Schneison <larson.schneider11@gmail.com>
2023-08-07 17:12:39 +0200

    feat: add scaling of day time cycle into the UI

--- src/main/java/de/uniks/stpmon/k/controller/SoundController.java
+++ src/main/java/de/uniks/stpmon/k/controller/SoundController.java
@@ -9,12 +9,19 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.util.StringConverter;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
+import java.util.function.Function;
 
-public class SoundController extends Controller{
+public class SoundController extends Controller {
 
+    /**
+     * Min value is 20 minutes
+     */
+    public static final int START_OFFSET = 0;
     @FXML
     public VBox soundScreen;
     @FXML
@@ -23,11 +30,14 @@ public class SoundController extends Controller{
     public Slider music;
     @FXML
     public CheckBox nightMode;
+    @FXML
+    public Slider dayCycle;
+    @FXML
+    public Text dayCycleLabel;
     @Inject
     Provider<HybridController> hybridControllerProvider;
     @Inject
     SettingsService settingsService;
-
 
     @Inject
     public SoundController() {
@@ -50,6 +60,40 @@ public class SoundController extends Controller{
         nightMode.setSelected(settingsService.getNightEnabled());
         listen(nightMode.selectedProperty(),
                 (observable, oldValue, newValue) -> settingsService.setNightEnabled(newValue));
+
+        // Day time cycle
+        dayCycle.setValue(settingsService.getDayTimeCycle());
+
+        Function<Double, String> converter =
+                (Double object) -> {
+                    int precision = (object.intValue() + START_OFFSET) % 3;
+                    return (object.intValue() + START_OFFSET) / 3 + ":" + (precision == 0 ? "00" : precision * 20) + "h";
+                };
+        dayCycleLabel.setText(translateString("day-cycle",
+                converter.apply(Double.valueOf(settingsService.getDayTimeCycle()))));
+        listen(dayCycle.valueProperty(),
+                (observable, oldValue, newValue) -> {
+                    float value = newValue.floatValue();
+                    if (value < 1) {
+                        dayCycle.setValue(1);
+                        return;
+                    }
+                    if (settingsService.setDayTimeCycle(value)) {
+                        dayCycleLabel.setText(translateString("day-cycle",
+                                converter.apply((double) value)));
+                    }
+                });
+        dayCycle.setLabelFormatter(new StringConverter<>() {
+            @Override
+            public String toString(Double object) {
+                return (object.intValue() + START_OFFSET) / 3 + "h";
+            }
+
+            @Override
+            public Double fromString(String string) {
+                return null;
+            }
+        });
 
         return parent;
     }
